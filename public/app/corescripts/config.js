4af8b5b700a48c3e8d5d7f4413102112a96f2a3f
Matan Kaminski <matan.kaminski.sdjasf@gmail.com>
2015-11-07 00:38:01 -0500

    Revert "testing script"
    This reverts commit d3d31554d92fdd423d45ada5bbff197ad288ab4b.

--- public/app/corescripts/config.js
+++ public/app/corescripts/config.js
@@ -1,13 +1,9 @@
 angular.module('MainApp')
-var backURL=process.env.BACKURL || '172.16.0.3'
-var frontURL=process.env.FRONTURL || '172.16.0.2'
 .constant('WizioConfig', {
-    baseAPIURL: backURL
-    //'http://beta-aqbvfzrbbx.elasticbeanstalk.com/api/',
+    baseAPIURL: 'http://beta-aqbvfzrbbx.elasticbeanstalk.com/api/',
 
     //This frontEndURL may seem pointless, its NOT, go to sharedfactories map function to find out why we need it
-    frontEndURL: frontURL
-    // 'http://172.16.0.2:3000/',
+    frontEndURL: 'http://172.16.0.2:3000/',
     //
     //angular app URLs - views
     //
