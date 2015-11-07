514b547484b49abd44e9680092d6ee4eadeb88f7
Chris Canal on Macbook Air <christophercanal4@gmail.com>
2015-11-07 00:39:57 -0500

    added facebook button

--- public/app/corescripts/config.js
+++ public/app/corescripts/config.js
@@ -1,13 +1,11 @@
 angular.module('MainApp')
-var backURL=process.env.BACKURL || '172.16.0.3'
-var frontURL=process.env.FRONTURL || '172.16.0.2'
 .constant('WizioConfig', {
-    baseAPIURL: backURL,
-    //'http://beta-aqbvfzrbbx.elasticbeanstalk.com/api/',
+    baseAPIURL: 'http://beta-aqbvfzrbbx.elasticbeanstalk.com/api/',
+    //baseAPIURL: 'http://beta-aqbvfzrbbx.elasticbeanstalk.com/api/',
 
     //This frontEndURL may seem pointless, its NOT, go to sharedfactories map function to find out why we need it
-    frontEndURL: frontURL,
-    // 'http://172.16.0.2:3000/',
+    frontEndURL: 'http://frontbeta-4qbeydmczn.elasticbeanstalk.com/',
+    // 'http://frontbeta-4qbeydmczn.elasticbeanstalk.com/',
     //
     //angular app URLs - views
     //
