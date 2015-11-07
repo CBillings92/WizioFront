adeecfc12c649696516f66808504e0144d728e0d
Matan Kaminski <matan.kaminski.sdjasf@gmail.com>
2015-11-07 11:09:25 -0500

    updating livebackend url

--- public/app/corescripts/config.js
+++ public/app/corescripts/config.js
@@ -1,9 +1,8 @@
 angular.module('MainApp')
 .constant('WizioConfig', {
-
-    //baseAPIURL: 'http://beta-aqbvfzrbbx.elasticbeanstalk.com/api/',
-    //baseAPIURL: 'http://beta-aqbvfzrbbx.elasticbeanstalk.com/api/',
-    baseAPIURL: 'http://172.16.0.3:4000/api/',
+    
+    baseAPIURL: 'liveserver-pu7s9gucbn.elasticbeanstalk.com/api/',
+    // baseAPIURL: 'http://172.16.0.3:4000/api/',
     //This frontEndURL may seem pointless, its NOT, go to sharedfactories map function to find out why we need it
     //frontEndURL: 'http://frontbeta-4qbeydmczn.elasticbeanstalk.com/',
 
