6561ca37dd2ca9a7897142ea35f472822f4cb6d6
Cameron Billings <Cameron.billings92@gmail.com>
2015-11-15 23:23:26 -0500

    URL changes for prod testing

--- public/app/corescripts/config.js
+++ public/app/corescripts/config.js
@@ -9,12 +9,12 @@ angular.module('MainApp')
 
     */
     //LOCAL URL
-    baseAPIURL: 'http://172.16.0.3:4000/api/',
+    //baseAPIURL: 'http://172.16.0.3:4000/api/',
 
 
 
     //LIVETESTER URL
-    //baseAPIURL: 'http://TESTENV-haje6dk4hy.elasticbeanstalk.com/api/',
+    baseAPIURL: 'http://TESTENV-haje6dk4hy.elasticbeanstalk.com/api/',
 
 
     //FRONT END SERVER URL
@@ -26,7 +26,7 @@ angular.module('MainApp')
     //The following line should be uncometed before pushing to the AWS elasticbeanstalk server
     //frontEndURL: 'http://frontbeta-4qbeydmczn.elasticbeanstalk.com/',
     //frontEndURL: 'http://MatanBetaTest-j5t4cpkddg.elasticbeanstalk.com/',
-    fonrtEndUrl: 'http://172.16.0.2:3000/',
+    //fonrtEndUrl: 'http://172.16.0.2:3000/',
 
 
 
