14911d90189e32994ddb8c1ed8ddee6a61e0ebcf
Drew <drewpom@icloud.com>
2015-11-26 20:21:18 -0500

    about styles

--- public/app/modules/aboutusapp/controllers/AboutListCtrl.js
+++ public/app/modules/aboutusapp/controllers/AboutListCtrl.js
@@ -5,27 +5,31 @@ angular.module("AboutUsApp")
             $scope.members = [{
                 name: "Chris Canal",
                 title: "CEO",
-                picture: "radish.png",
+                picture: "chris.jpg",
                 twitter: "chriscanal4",
                 linkedin: "chriscanal",
+                email: "",
             }, {
                 name: "Cameron Billings",
                 title: "CTO",
-                picture: "radish.png",
+                picture: "chris.jpg",
                 twitter: "CamBillings_",
                 linkedin: "cameronbillings",
+                email: "",
             }, {
                 name: "Devon Billings",
                 title: "COO/CMO",
-                picture: "radish.png",
+                picture: "chris.jpg",
                 twitter: "dsgrod",
                 linkedin: "dsgrod",
+                email: "",
             }, {
                 name: "Trent Duffy",
                 title: "CMS-Cheif of Misc Stuff",
-                picture: "radish.png",
+                picture: "chris.jpg",
                 twitter: "trentolol",
                 linkedin: "trentduffy",
+                email: "",
             }];
 
             
