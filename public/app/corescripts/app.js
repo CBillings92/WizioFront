f991fd3384b9f943652fe27bef5f30258de3a462
U-DESKTOP-ENM503I\Camer <cameron.billings92@gmail.com>
2015-12-07 18:40:42 -0500

    removed console log in app.js

--- public/app/corescripts/app.js
+++ public/app/corescripts/app.js
@@ -144,7 +144,6 @@ angular.module('MainApp', [
             var tokenIsExp = null;
             if(token){
                 tokenIsExp = TokenSvc.checkExp();
-                console.dir(TokenSvc.checkExp());
             }
 
             //if no token exists, assign isLoggedIn to false
