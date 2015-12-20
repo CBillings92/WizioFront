6cae8cad49a4f7cae5519e6d301f172f72d273be
Cameron Billings <Cameron.billings92@gmail.com>
2015-12-20 02:11:50 -0500

    apartment claim search form base functionality done on front end

--- public/app/modules/unitapp/controllers/UnitClaimSearchCtrl.js
+++ public/app/modules/unitapp/controllers/UnitClaimSearchCtrl.js
@@ -10,26 +10,36 @@ angular.module('UnitApp')
         'ApartmentClaimGetSetSvc',
         'modalData',
         'UnitCreateSvc',
-        function($scope, $modalInstance, $sessionStorage, SmartSearchSvc, FlexGetSetSvc, ApartmentClaimGetSetSvc, modalData, UnitCreateSvc) {
+        'lodash',
+        function($scope, $modalInstance, $sessionStorage, SmartSearchSvc, FlexGetSetSvc, ApartmentClaimGetSetSvc, modalData, UnitCreateSvc, lodash) {
             $scope.modalOptions = modalData;
             //create constructor function for an Apartment
             var Apartment = function(trackingID, address, unitNum) {
                 this.address = address;
-                this.unitNumArray = [];
+                this.unitNum;
             };
             //Array that will hold apartment reference objects to turn into real
             //apartment objects with unit numbers
-            $scope.apartmentReferenceArray = [];
+            $scope.finalApartmentArray = [];
             //Adding the first apartment to the reference array to kick off the form
             //when the user first loads the modal
-            $scope.apartmentReferenceArray[0] = new Apartment(null, null);
-            var finalApartmentArray = [];
+            $scope.finalApartmentArray[0] = [(new Apartment(null, null))];
+
             //smart bar code
             $scope.getLocation = function(val) {
                 /*
                     pass second string variable to specify where to store googel geocoding API results from search bar.
                 */
                 return SmartSearchSvc.smartSearch(val, 'Staging-ApartmentClaims');
+            };
+            //on selecting the add address button
+            $scope.addAddress = function() {
+                //create a new, blank Apartment object
+                var apartment = new Apartment(null, null);
+                //store taht apartment object in an array
+                var apartmentArray = [apartment];
+                //push that array into the referenceArray array
+                $scope.finalApartmentArray.push(apartmentArray);
             };
             /*
                 addressEntered : ON-BLUR - when address bar loses focus get the last object
@@ -38,45 +48,66 @@ angular.module('UnitApp')
                 Store that object in ApartmentClaims array in sessionStorage
             */
             $scope.addressEntered = function(apartmentIndex){
-                UnitCreateSvc.parseGeocodeData($scope.apartmentReferenceArray[apartmentIndex].address, null, function(err, apartment){
+                UnitCreateSvc.parseGeocodeData($scope.finalApartmentArray[apartmentIndex].address, null, function(err, apartment){
                     //store apartment object from geocoder in fullApartment object
-                    $scope.apartmentReferenceArray[apartmentIndex].push(apartment);
-                    //move the unitNumArray into the fullApartment object
-                    /*
-                    $scope.apartmentReferenceArray[apartmentIndex].fullApartment.unitNums =
-                    $scope.apartmentReferenceArray[apartmentIndex].unitNumArray;
-                    */
+                    $scope.finalApartmentArray[apartmentIndex].fullApartment = apartment;
                     //set this object in sessionStorage
                     ApartmentClaimGetSetSvc.set(apartment, 'ApartmentClaims');
                 });
             };
+            $scope.unitEntered  = function(apartmentIndex){
+                //get the last unit entered into the apartment array
+                var lastUnitEntered = $scope.finalApartmentArray[apartmentIndex].pop();
+                //get the full apartment object that we got from the geocoder
+                var fullApartment = $scope.finalApartmentArray[apartmentIndex].fullApartment;
+                //create an empty object for the impending clone war..er...object clone
+                var objectClone = {};
+                //initialize i variable for for loop
+                var i;
+                //store the keys of the fullApartment object in a variable
+                var keys = Object.keys(fullApartment);
+                //loop through all of the keys and make a clone object
+                for(i = 0; i < keys.length; i++){
+                    objectClone[keys[i]] = fullApartment[keys[i]];
+                }
+                //store the objectClone of the fullApartment on the current unit
+                lastUnitEntered.fullApartment = objectClone;
+                //store the unitNumber on the last entered unit in the fullApartment
+                lastUnitEntered.fullApartment.unitNum = lastUnitEntered.unitNum;
+                //push this new object back onto the array we popped it off of
+                $scope.finalApartmentArray[apartmentIndex].push(lastUnitEntered);
+            };
             $scope.addUnit = function(apartmentIndex) {
-                $scope.apartmentReferenceArray[apartmentIndex].unitNumArray.push(null);
-                console.dir($scope.apartmentReferenceArray);
+                $scope.finalApartmentArray[apartmentIndex].push(new Apartment(null,null));
+
+                //$scope.finalApartmentArray[($scope.finalApartmentArray.length - 1)].unitNum = null;
+                //$scope.finalApartmentArray[apartmentIndex].unitNumArray.push(null);
+                console.dir($scope.finalApartmentArray);
 
 
+            };
+            $scope.removeUnit = function(apartmentArrayIndex, unitIndex){
+                console.dir($scope.finalApartmentArray[apartmentArrayIndex]);
+                //console.dir($scope.finalApartmentArray[apartmentArrayIndex][unitIndex]);
+                console.dir(unitIndex);
+                $scope.finalApartmentArray[apartmentArrayIndex].splice(unitIndex, 1);
             };
             $scope.addRange = function(apartmentIndex) {
                 for (var i = 0; i < $scope.highNum; i++) {
-                    $scope.apartmentReferenceArray[apartmentIndex].unitNumArray.push(null);
+                    $scope.finalApartmentArray[apartmentIndex].unitNumArray.push(null);
                 }
-            };
-            $scope.addAddress = function() {
-                var apartment = new Apartment(null, null);
-                $scope.apartmentReferenceArray.push(apartment);
             };
 
             $scope.search = function() {
-                var finalArray = [];
-                for(i = 0; i < $scope.apartmentReferenceArray.length; i++){
-                    for(j = 0; j < $scope.apartmentReferenceArray[i].unitNumArray.length; j++){
-                        var apartment = $scope.apartmentReferenceArray[i].fullApartment;
-                        console.dir($scope.apartmentReferenceArray[i].unitNumArray[j]);
-                        apartment.unitNum = $scope.apartmentReferenceArray[i].unitNumArray[j];
-                        console.dir(apartment);
-                    }
-                    //console.dir(finalArray);
+                for(var i = 0; i < $scope.finalApartmentArray.length; i++){
+                    delete $scope.finalApartmentArray[i].address;
+                    delete $scope.finalApartmentArray[i].fullApartment;
+
                 }
+                var finalData = lodash.flatten(lodash.map($scope.finalApartmentArray, lodash.values));
+                finalData = lodash.pluck(finalData, 'fullApartment');
+                console.dir(finalData);
+
                 //search database
 
                 //navigate to apartmentEdit form
