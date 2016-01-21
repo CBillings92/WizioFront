fb26c215f657b222768c7e4a02751b1f66792c84
Cameron Billings <Cameron.billings92@gmail.com>
2016-01-20 20:30:22 -0500

    fixed dashbaord bug

--- public/app/modules/accountapp/dashboardapp/controllers/DashboardUserInfoCtrl.js
+++ public/app/modules/accountapp/dashboardapp/controllers/DashboardUserInfoCtrl.js
@@ -1,10 +1,8 @@
 angular.module('AccountApp')
 .controller('DashboardUserInfoCtrl', [
     '$scope',
-    function($scope){
-        $scope.$on('AccountInfoBroadcast', function(event, data){
-            $scope.accountInfo = data;
-        });
-
+    'TokenSvc',
+    function($scope, TokenSvc){
+        $scope.accountInfo = TokenSvc.decode();
     }
 ]);
