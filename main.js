729d63d46760ce16faee54ccff25d99fe388312e
U-DESKTOP-ENM503I\Camer <cameron.billings92@gmail.com>
2016-01-30 12:08:36 -0500

    merging

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var config = require('./config/config');
 
 var routes = require('./routes/index');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
