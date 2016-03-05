5acf79d2b75544380ae5ac88246a2798309dc881
trentduffy <ct_trentduffy@me.com>
2016-03-05 15:04:47 -0500

    leaseform updates

--- public/app/directives/favoritesdirv.js
+++ public/app/directives/favoritesdirv.js
@@ -8,3 +8,17 @@ angular.module('Directives')
       }
   };
 }])
+.directive('currency', ['$filter', function ($filter) {
+    return {
+        require: 'ngModel',
+        scope: false,
+        link: function (elem, $scope, attrs, ngModel) {
+            ngModel.$formatters.push(function (val) {
+                return $filter('currency')(val)
+            });
+            ngModel.$parsers.push(function (val) {
+                return val.replace(/[\$,]/, '')
+            });
+        }
+    }
+}]);
