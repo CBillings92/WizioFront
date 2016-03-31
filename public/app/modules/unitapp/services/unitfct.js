88dd630481d2081188b9a87d903c715c987a8f86
Cameron Billings <Cameron.billings92@gmail.com>
2016-03-30 23:17:15 -0400

    figured out SVGs

--- public/app/modules/unitapp/services/unitfct.js
+++ public/app/modules/unitapp/services/unitfct.js
@@ -14,7 +14,258 @@ angular.module('UnitApp')
             makeCurrentListing: ['Yes', 'No'],
             pmBusinessOrPerson: ['Business', 'Person']
         };
+        var icons = {
+            beds:{
+                baseUrl: "",
+                blackUrl: "public/assets/icons/bed-black.svg",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
 
+                },
+            },
+            baths:{
+                baseUrl: "",
+                blackUrl: "public/assets/icons/bath-black.svg",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            laundry:{
+                baseUrl: "",
+                blackUrl: "public/assets/icons/laundry-black.svg",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            dishwasher:{
+                baseUrl: "",
+                blackUrl: "public/assets/icons/dishwasher-black.svg",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            ac:{
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            electricity: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            gas: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            heat: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            hotwater: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            watersewer: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            wifi: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            pets: {
+                baseUrl: "",
+                blackUrl: "public/assets/icons/pets-black.svg",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            elevator: {
+                baseUrl: "",
+                blackUrl: "public/assets/icons/elevator-black.svg",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            cable: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            garbagedisposal: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            trashremoval: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            snowremoval: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            fireplace: {
+                baseUrl: "",
+                blackUrl: "public/assets/icons/fireplace-black.svg",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            garage: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            parking: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            concierge: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            superintendent: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            pool: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            porch: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+            smoking: {
+                baseUrl: "",
+                blackUrl: "",
+                blueUrl: "",
+                orangeUrl: "",
+                grayUrl: "",
+                description: {
+
+                },
+            },
+        };
         var apartmentExisted = function(newApartment, response){
             var user = TokenSvc.decode();
             newApartment.apartmentData.id = response.apartment.id;
