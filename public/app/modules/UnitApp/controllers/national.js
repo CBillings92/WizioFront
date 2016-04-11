69c8533e40c20fe8b3e66c6ec894237fc86b979f
U-DESKTOP-ENM503I\Camer <cameron.billings92@gmail.com>
2016-04-11 01:06:38 -0400

    magic!

--- public/app/modules/UnitApp/controllers/national.js
+++ public/app/modules/UnitApp/controllers/national.js
@@ -1,80 +1,84 @@
 angular.module('UnitApp')
-   .controller('NationalCtrl', [
-       '$scope',
-       '$state',
-       '$modal',
-       '$location',
-       '$sessionStorage',
-       '$sce',
-       '$resource',
-       function (
-           $scope,
-           $state,
-           $modal,
-           $location,
-           $sessionStorage,
-           $sce,
-           $resource
-       ) {
-                $scope.media = {};
-                //PLURAL
-               var vrphotos = [];
-                var whichUnit = 1;
-               //PLURAL
+    .controller('NationalCtrl', [
+        '$scope',
+        '$state',
+        '$modal',
+        '$location',
+        '$sessionStorage',
+        '$sce',
+        '$resource',
+        function(
+            $scope,
+            $state,
+            $modal,
+            $location,
+            $sessionStorage,
+            $sce,
+            $resource
+        ) {
+            $scope.media = {};
+            //PLURAL
+            var vrphotos = [];
+            var whichUnit = 1;
+            //PLURAL
 
 
-               function updateUnitPics() {
-                    console.log("fuck you");
-                        if (whichUnit == 1) {
-                            vrphotos = [];
-                            vrphotos.push({
-                                title: "Place 2",
-                                link: "https://www.bubl.io/experiences/156ea094-059c-401f-b9c7-a8fe5f5d5678"
-                            });
+            function updateUnitPics() {
+                console.log("fuck you");
+                if (whichUnit == 1) {
+                    vrphotos = [];
+                    vrphotos.push({
+                        title: "Place 2",
+                        link: "https://www.bubl.io/experiences/156ea094-059c-401f-b9c7-a8fe5f5d5678"
+                    });
 
-                            vrphotos.push({
-                                title: "Cam is Sexy",
-                                link: "https://www.bubl.io/experiences/68c7bc43-faae-4708-b581-075dca0b0733"
-                            });
-                            return;
-                        } else if (whichUnit == 2) {
-                            console.log("fuck you more");
+                    vrphotos.push({
+                        title: "Cam is Sexy",
+                        link: "https://www.bubl.io/experiences/68c7bc43-faae-4708-b581-075dca0b0733"
+                    });
+                    handleMedia();
+                    return;
+                } else if (whichUnit == 2) {
+                    console.log("fuck you more");
 
-                            vrphotos = [];
+                    vrphotos = [];
 
-                            vrphotos.push({
-                                title: "Cam is Sexy",
-                                link: "https://www.bubl.io/experiences/68c7bc43-faae-4708-b581-075dca0b0733"
-                            });
-                            return;
-                        }
-                        return;
+                    vrphotos.push({
+                        title: "Cam is Sexy",
+                        link: "https://www.bubl.io/experiences/68c7bc43-faae-4708-b581-075dca0b0733"
+                    });
+                    handleMedia();
+                    return;
                 }
+                return;
+            }
 
-              updateUnitPics();
+            updateUnitPics();
+            handleMedia();
+            function handleMedia() {
+                //PLURAL
+                $scope.media.vrphotos = vrphotos;
+                var photoIndex = 0;
+                //NON-PLURAL
+                $scope.photoUrl = $scope.media.vrphotos[photoIndex].link;
+                $scope.changePhoto = function(photoIndex) {
+                    //NON-PLURAL
+                    $scope.photoUrl = $scope.media.vrphotos[photoIndex].link;
+                };
+            }
 
+            $scope.trust = $sce;
+            //FIXME
+            $scope.mediaTab = 'unitPhotos';
 
-               //PLURAL
-               $scope.media.vrphotos = vrphotos;
-               var photoIndex = 0;
-               //NON-PLURAL
-               $scope.photoUrl = $scope.media.vrphotos[photoIndex].link;
-               $scope.changePhoto = function(photoIndex) {
-                   //NON-PLURAL
-                   $scope.photoUrl = $scope.media.vrphotos[photoIndex].link;
-               };
-               $scope.trust = $sce;
-               //FIXME
-               $scope.mediaTab = 'unitPhotos';
+            $scope.updateUnit1 = function() {
+                whichUnit = 1;
+                updateUnitPics();
+            };
+            $scope.updateUnit2 = function() {
+                whichUnit = 2;
+                updateUnitPics();
+            };
 
-               $scope.updateUnit1 = function() {
-                   whichUnit = 1;
-                   updateUnitPics();
-               };
-               $scope.updateUnit2 = function() {
-                   whichUnit = 2;
-                   updateUnitPics();
-               };
-
-       }
-   ]);
+        }
+    ]);
