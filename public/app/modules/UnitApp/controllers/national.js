173cc19f7eed78859c8bef203c73bffd79f2a32b
trentduffy <ct_trentduffy@me.com>
2016-04-11 01:01:58 -0400

    guess what? still not working

--- public/app/modules/UnitApp/controllers/national.js
+++ public/app/modules/UnitApp/controllers/national.js
@@ -1,140 +1,80 @@
 angular.module('UnitApp')
-    .controller('NationalCtrl', [
-        '$scope',
-        '$state',
-        '$modal',
-        '$location',
-        '$sessionStorage',
-        '$sce',
-        '$resource',
-        'lodash',
-        'ApartmentGetSetSvc',
-        'UnitResource',
-        'MapFct',
-        'TokenSvc',
-        'FlexGetSetSvc',
-        'RerouteGetSetSvc',
-        'FavoriteModel',
-        'MediaModel',
-        'ModalSvc',
-        'WizioConfig',
-        'SearchFct',
-        'moment',
-        'UnitFct',
-        function(
-            $scope,
-            $state,
-            $modal,
-            $location,
-            $sessionStorage,
-            $sce,
-            $resource,
-            lodash,
-            ApartmentGetSetSvc,
-            UnitResource,
-            MapFct,
-            TokenSvc,
-            FlexGetSetSvc,
-            RerouteGetSetSvc,
-            FavoriteModel,
-            MediaModel,
-            ModalSvc,
-            WizioConfig,
-            SearchFct,
-            moment,
-            UnitFct
-        ) {
-                var vrphotos = [];
-                // var whichUnit = 1;
-                vrphotos.push({label:"Cam is Sexy", link:"https://www.bubl.io/experiences/68c7bc43-faae-4708-b581-075dca0b0733"});
+   .controller('NationalCtrl', [
+       '$scope',
+       '$state',
+       '$modal',
+       '$location',
+       '$sessionStorage',
+       '$sce',
+       '$resource',
+       function (
+           $scope,
+           $state,
+           $modal,
+           $location,
+           $sessionStorage,
+           $sce,
+           $resource
+       ) {
+                $scope.media = {};
+                //PLURAL
+               var vrphotos = [];
+                var whichUnit = 1;
+               //PLURAL
 
-                var media = lodash.groupBy(result.Apartment.Media, 'type');
 
-                $scope.media = media;
-                $scope.media.vrphoto = vrphotos;
-                var photoIndex = 0;
-                $scope.photoUrl = $scope.media.vrphoto[photoIndex].link;
-                $scope.changePhoto = function(photoIndex) {
-                    $scope.photoUrl = $scope.media.vrphoto[photoIndex].link;
-                };
-                $scope.trust = $sce;
-                //FIXME
-                $scope.mediaTab = 'unitPhotos';
-                $scope.selectMediaTab = function(tab) {
-                    if (tab === 'unitVideos') {
-                        if (vrvideos.length !== 1) {
-                            var signUpErrorModalOptions = {
-                                closeButtonText: "Close",
-                                actionButtonText: "OK",
-                                headerText: "No 360 Video",
-                                bodyText: 'Sorry! This unit does not have a 360 video tour just yet.'
-                            };
-                            ModalSvc.showModal({}, signUpErrorModalOptions)
-                                .then(function(result) {
-                                    return;
-                                });
+               function updateUnitPics() {
+                    console.log("fuck you");
+                        if (whichUnit == 1) {
+                            vrphotos = [];
+                            vrphotos.push({
+                                title: "Place 2",
+                                link: "https://www.bubl.io/experiences/156ea094-059c-401f-b9c7-a8fe5f5d5678"
+                            });
 
-                        } else {
-                            $scope.mediaTab = tab;
+                            vrphotos.push({
+                                title: "Cam is Sexy",
+                                link: "https://www.bubl.io/experiences/68c7bc43-faae-4708-b581-075dca0b0733"
+                            });
+                            return;
+                        } else if (whichUnit == 2) {
+                            console.log("fuck you more");
+
+                            vrphotos = [];
+
+                            vrphotos.push({
+                                title: "Cam is Sexy",
+                                link: "https://www.bubl.io/experiences/68c7bc43-faae-4708-b581-075dca0b0733"
+                            });
+                            return;
                         }
-                    } else {
+                        return;
+                }
 
-                        $scope.mediaTab = tab;
-                    }
-            };
+              updateUnitPics();
 
-            });
 
-            //HELPER FUNCTION -- modal creation function
-            var modalDefaults = function(templateUrl, controller, accountType, apartmentData) {
-                return {
-                    backdrop: true,
-                    keyboard: true,
-                    modalFade: true,
-                    templateUrl: templateUrl,
-                    controller: controller,
-                    resolve: {
-                        modalData: function() {
-                            return apartmentData;
-                        }
-                    }
-                };
-            };
-            var authViews = WizioConfig.AccountAuthViewsURL;
-            var modalDefaultsLogin = modalDefaults(authViews + 'Login.html', 'AuthLoginModalCtrl');
-            //APPLY to the apartment
-            $scope.applyToApartment = function() {
-                var modalDefaultsApplication = modalDefaults(WizioConfig.ApplicationFormViewsURL + 'contactRepForm.html', 'ContactRepFormCtrl', 'md', $scope.listing);
-                //check if token is expired, if so route to login
-                ModalSvc.showModal(modalDefaultsApplication, {}).then(function(result) {
-                    if (result === 'submit') {
+               //PLURAL
+               $scope.media.vrphotos = vrphotos;
+               var photoIndex = 0;
+               //NON-PLURAL
+               $scope.photoUrl = $scope.media.vrphotos[photoIndex].link;
+               $scope.changePhoto = function(photoIndex) {
+                   //NON-PLURAL
+                   $scope.photoUrl = $scope.media.vrphotos[photoIndex].link;
+               };
+               $scope.trust = $sce;
+               //FIXME
+               $scope.mediaTab = 'unitPhotos';
 
-                    }
-                    // $state.go('Account.Dashboard.Main');
-                });
-                // if (TokenSvc.checkExp()) {
-                //     TokenSvc.deleteToken();
-                //
-                //     ModalSvc.showModal(modalDefaultsLogin, {}).then(function(result) {
-                //         //store the current apartment in sessionStorage with the
-                //         //appropriate session storage variable
-                //         if (result) {
-                //             FlexGetSetSvc.set($scope.apartment, "ApartmentApplyingTo");
-                //             ModalSvc.showModal(modalDefaultsApplication, {}).then(function(result) {
-                //                 $state.go('Account.Dashboard.Main');
-                //             });
-                //
-                //         }
-                //
-                //     });
-                // } else {
-                //     //store the current apartment in sessionStorage with the
-                //     //appropriate session storage variable
-                //     FlexGetSetSvc.set($scope.apartment, "ApartmentApplyingTo");
-                //
-                // }
-            };
+               $scope.updateUnit1 = function() {
+                   whichUnit = 1;
+                   updateUnitPics();
+               };
+               $scope.updateUnit2 = function() {
+                   whichUnit = 2;
+                   updateUnitPics();
+               };
 
-            //LOAD APARTMENT DATA end
-        }
-    ]);
+       }
+   ]);
