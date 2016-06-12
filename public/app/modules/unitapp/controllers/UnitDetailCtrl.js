3850fbd56a40bee22bf27b259112bcbdf6822312
Cameron Billings <Cameron.billings92@gmail.com>
2016-06-12 09:41:37 -0400

    our own VR player working on the listing pages with our own hosted content

--- public/app/modules/unitapp/controllers/UnitDetailCtrl.js
+++ public/app/modules/unitapp/controllers/UnitDetailCtrl.js
@@ -42,97 +42,66 @@ angular.module('UnitApp')
         ) {
             var user = TokenSvc.decode();
             $scope.listing = {};
-            function setDataForPage(data){
+
+            function setDataForPage(data) {
                 var dataForPage = UnitDetailsFct.setDataForPage(data);
                 $scope.apartment = dataForPage.apartment;
                 $scope.listing = dataForPage.listing;
                 $scope.features = dataForPage.features;
                 return;
             }
+
             function setHardCodedFloorplans() {
-                if ($scope.apartment.street === "1040 North Quincy Street") {
-                    switch ($scope.apartment.unitNum) {
-                        case "406":
-                            $scope.floorplan = "https://s3.amazonaws.com/wiziouservideos/LG-1b1d1s2b.png";
-                            break;
-                        case "209":
-                            // $scope.floorplan = false;
-                            break;
-                        default:
-                            // $scope.floorplan = false;
-                    }
-                } else if ($scope.apartment.street === "1020 North Quincy Street") {
-                    switch ($scope.apartment.unitNum) {
-                        case "908":
-                            $scope.floorplan = "https://s3.amazonaws.com/wiziouservideos/1020-2b2b.png";
-                            break;
-                        case "1013":
-                            $scope.floorplan = "https://s3.amazonaws.com/wiziouservideos/1020-2b1b.png";
-                            break;
-                        case "616":
-                            $scope.floorplan = false;
-                            break;
-                        case "619":
-                            $scope.floorplan = false;
-                            break;
-                        default:
-                            $scope.floorplan = false;
-                    }
-                } else {
-                    $scope.floorplan = false;
-                }
+
             }
+
             function setVideoAndPhotos(data) {
-                {
-                    var vrphotos = [];
-                    var vrvideos = [];
-                    for (var i = 0; i < data.Apartment.Media.length; i++) {
-                        if (data.Apartment.Media[i].type === 'vrphoto') {
-                            vrphotos.push(data.Apartment.Media[i]);
-                            //        console.log(result.Apartment.Media[i]);
-                        } else {
-                            vrvideos.push(data.Apartment.Media[i]);
-                        }
-                    }
-                    var media = lodash.groupBy(data.Apartment.Media, 'type');
-                    $scope.media = media;
-                    $scope.media.vrphoto = vrphotos;
-                    var photoIndex = 0;
-                    $scope.photoUrl = $scope.media.vrphoto[photoIndex].link;
-                    $scope.changePhoto = function(photoIndex) {
-                        $scope.photoUrl = $scope.media.vrphoto[photoIndex].link;
-                    };
-                    $scope.trust = $sce;
-                    //FIXME
-                    $scope.mediaTab = 'unitPhotos';
-                    $scope.selectMediaTab = function(tab) {
-                        if (tab === 'unitVideos') {
-                            if (vrvideos.length !== 1) {
-                                var signUpErrorModalOptions = {
-                                    closeButtonText: "Close",
-                                    actionButtonText: "OK",
-                                    headerText: "No 360 Video",
-                                    bodyText: 'Sorry! This unit does not have a 360 video tour just yet.'
-                                };
-                                ModalSvc.showModal({}, signUpErrorModalOptions)
+                var photoIndex = 0;
+                var setPhotosObj = UnitDetailsFct.setPhotos(data);
+                console.dir(setPhotosObj);
+                var media = setPhotosObj.media;
+                $scope.photoUrl = setPhotosObj.initialPhotoUrl;
+                // $scope.media.vrphoto = vrphotos;
+                $scope.$broadcast('IMGLOAD', {media: media});
+                // $scope.media.vrphoto = vrphotos;
+                $scope.changePhoto = function(photoIndex) {
+                    $scope.photoUrl = media.vrphoto[photoIndex].awsurl;
+                    console.dir($scope.photoUrl);
+                    $scope.$broadcast('CHANGE', {});
+                };
+
+                $scope.trust = $sce;
+                //FIXME
+                $scope.mediaTab = 'unitPhotos';
+                $scope.selectMediaTab = function(tab) {
+                    if (tab === 'unitVideos') {
+                        if (vrvideos.length !== 1) {
+                            var signUpErrorModalOptions = {
+                                closeButtonText: "Close",
+                                actionButtonText: "OK",
+                                headerText: "No 360 Video",
+                                bodyText: 'Sorry! This unit does not have a 360 video tour just yet.'
+                            };
+                            ModalSvc.showModal({}, signUpErrorModalOptions)
                                 .then(function(result) {
                                     return;
                                 });
 
-                            } else {
-                                $scope.mediaTab = tab;
-                            }
                         } else {
                             $scope.mediaTab = tab;
                         }
-                    };
-                }
+                    } else {
+                        $scope.mediaTab = tab;
+                    }
+                };
             }
             //
             LeaseModel.api('get', $state.params.id)
-                .then(function(result){
+                .then(function(result) {
+                    var street = result.Apartment.street;
+                    var unitnum = result.Apartment.unitNum;
                     setDataForPage(result);
-                    setHardCodedFloorplans(result);
+                    $scope.floorplan = UnitDetailsFct.setFloorPlan(street, unitnum);
                     setVideoAndPhotos(result);
                     return;
                 });
--- public/app/modules/unitapp/services/unitdetails.fct.js
+++ public/app/modules/unitapp/services/unitdetails.fct.js
@@ -2,7 +2,8 @@ angular.module('UnitApp')
     .factory('UnitDetailsFct', [
         'ApartmentGetSetSvc',
         'UnitFct',
-        function unitDetailsFct(ApartmentGetSetSvc,UnitFct) {
+        'lodash',
+        function unitDetailsFct(ApartmentGetSetSvc, UnitFct, lodash) {
             function setDataForPage(dataFromAPI) {
                 var listing, apartment, features;
 
@@ -25,8 +26,86 @@ angular.module('UnitApp')
                 };
                 return dataForPage;
             }
+
+            function setFloorPlan(street, unitnum) {
+                var floorplan;
+                if (street === "1040 North Quincy Street") {
+                    switch (street) {
+                        case "406":
+                            floorplan = "https://s3.amazonaws.com/wiziouservideos/LG-1b1d1s2b.png";
+                            break;
+                        case "209":
+                            // floorplan = false;
+                            break;
+                        default:
+                            // floorplan = false;
+                    }
+                } else if (street === "1020 North Quincy Street") {
+                    switch (street) {
+                        case "908":
+                            floorplan = "https://s3.amazonaws.com/wiziouservideos/1020-2b2b.png";
+                            break;
+                        case "1013":
+                            floorplan = "https://s3.amazonaws.com/wiziouservideos/1020-2b1b.png";
+                            break;
+                        case "616":
+                            floorplan = false;
+                            break;
+                        case "619":
+                            floorplan = false;
+                            break;
+                        default:
+                            floorplan = false;
+                    }
+                } else {
+                    floorplan = false;
+                }
+                return floorplan;
+            }
+
+            function setMap() {
+
+            }
+            function selectMediaTab(tab) {
+                var mediaTab = 'unitPhotos';
+                if (tab === 'unitVideos') {
+                    if (vrvideos.length !== 1) {
+                        var signUpErrorModalOptions = {
+                            closeButtonText: "Close",
+                            actionButtonText: "OK",
+                            headerText: "No 360 Video",
+                            bodyText: 'Sorry! This unit does not have a 360 video tour just yet.'
+                        };
+                        ModalSvc.showModal({}, signUpErrorModalOptions)
+                        .then(function(result) {
+                            return;
+                        });
+
+                    } else {
+                        $scope.mediaTab = tab;
+                    }
+                } else {
+                    $scope.mediaTab = tab;
+                }
+            }
+            function setPhotos(data) {
+                var photoIndex = 0;
+                var vrphotos = [], vrvideos = [];
+                console.dir("HI");
+                var media = lodash.groupBy(data.Apartment.Media, 'type');
+                console.dir(media);
+                var initialPhotoUrl = media.vrphoto[photoIndex].awsurl;
+                console.dir(initialPhotoUrl);
+                return {
+                    media: media,
+                    initialPhotoUrl: initialPhotoUrl
+                };
+            }
             return {
-                setDataForPage: setDataForPage
+                setDataForPage: setDataForPage,
+                setFloorPlan: setFloorPlan,
+                setMap: setMap,
+                setPhotos: setPhotos,
             };
         }
     ]);
