dc949287ed3599e8afbfb8b496807221e649fa67
U-DESKTOP-ENM503I\Camer <cameron.billings92@gmail.com>
2016-06-17 07:53:02 -0400

    close modal appropriately

--- public/app/modules/aboutusapp/controllers/apirequest.modal.ctrl.js
+++ public/app/modules/aboutusapp/controllers/apirequest.modal.ctrl.js
@@ -9,6 +9,7 @@ angular.module('AboutUsApp')
                 $resource(WizioConfig.baseAPIURL + 'vr/requestmoreinfo')
                     .save({apirequest: $scope.apirequest}, function(response){
                         alert("Thanks for contacting us! We'll get back to you as soon as we can.");
+                        $uibModalInstance.close();
                     });
             };
             $scope.closeModal = function() {
