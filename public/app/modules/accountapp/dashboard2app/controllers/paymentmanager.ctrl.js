fc373f1994d92987c7a1d765fa072e51c938fab8
U-DESKTOP-ENM503I\Camer <cameron.billings92@gmail.com>
2016-08-13 21:58:44 -0400

    new dashboard branch

---
+++ public/app/modules/accountapp/dashboard2app/controllers/paymentmanager.ctrl.js
@@ -0,0 +1,6 @@
+angular.module('AccountApp')
+    .controller('PaymentManagerCtrl', ['$scope', function($scope) {
+        //get user plan
+        $scope.plan = 'Premium';
+        $scope.costPerMonth = 99.00;
+    }])
