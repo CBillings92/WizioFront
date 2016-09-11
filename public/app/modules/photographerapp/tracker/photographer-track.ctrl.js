a9704674c242ae3a1d01de101800f45af094450d
U-DESKTOP-ENM503I\Camer <cameron.billings92@gmail.com>
2016-09-11 15:13:05 -0400

    wireframes in html for tracking page

--- public/app/modules/photographerapp/tracker/photographer-track.ctrl.js
+++ public/app/modules/photographerapp/tracker/photographer-track.ctrl.js
@@ -1,4 +1,8 @@
+/*
+    For tracking photographer activities during a photo shoot at a single apartment.
+    Controls the photographer-track.view.html form
+*/
 angular.module('PhotographerApp')
     .controller('PhotographerTrackCtrl', ['$scope', function($scope) {
-      alert('tears');
+        
   }]);
