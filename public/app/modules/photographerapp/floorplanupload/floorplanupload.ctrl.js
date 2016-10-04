28c9c0a485181d20ac7e018508dbb8957f991a2e
U-DESKTOP-ENM503I\Camer <cameron.billings92@gmail.com>
2016-10-04 18:00:51 -0400

    floorplan upload tool beginning

--- public/app/modules/photographerapp/floorplanupload/floorplanupload.ctrl.js
+++ public/app/modules/photographerapp/floorplanupload/floorplanupload.ctrl.js
@@ -1,20 +1,21 @@
 angular.module('PhotographerApp')
-    .controller('FloorPlanUploadCtrl', ['$scope', function($scope) {
+    .controller('FloorPlanUploadCtrl', ['$scope', '$resource', 'WizioConfig', function($scope, $resource, WizioConfig) {
+        var apartmentAddress, floorPlanModel, apiurl;
+
+        apiurl = WizioConfig.baseAPIURL;
+        //config the AWS object in the global scope
         AWS.config.update({
             accessKeyId: 'AKIAJ3OKI45M7I25XVNQ',
             secretAccessKey: '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'
-
         });
-        // AWS.config.region = 'us-east-1';
+        //config the AWS S3 object for upload
         var bucket = new AWS.S3({
             endpoint: 'http://cdn.wizio.co',
             s3BucketEndpoint: true,
             region: 'us-east-1',
-            // params: {
-            //     Bucket: 'equirect-photos'
-            // }
         });
 
+        //vanilla JS for uploading the photo
         var fileChooser = document.getElementById('file-chooser');
         var button = document.getElementById('upload-button');
         var results = document.getElementById('results');
@@ -31,9 +32,22 @@ angular.module('PhotographerApp')
                 };
                 bucket.putObject(params, function(err, data) {
                     results.innerHTML = err ? 'ERROR!' : 'UPLOADED.';
+                    console.dir(data);
                 });
             } else {
                 results.innerHTML = 'Nothing to upload.';
             }
         }, false);
+
+        //send apartemnt address and unit number to the backend
+        function createAddress(){
+            $resource(apiurl + '/photographer/yadayadayada', {apartmentAddress: '@apartmentAddress', floorPlanModel: '@floorPlanModel'})
+            .save({apartmentAddress: apartmentAddress, floorPlanModel: floorPlanModel}, function(response){
+                console.dir('inResponse');
+            })
+        }
+        //get the google maps corrected data
+        //save it to the database
+        //return the address
+
     }])
