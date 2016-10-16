62a0ac0e33da5a8e5fe232c8642f71136413ae9d
U-DESKTOP-ENM503I\Camer <cameron.billings92@gmail.com>
2016-10-16 13:01:03 -0400

    percentages calculated on mousce click

--- public/app/modules/photographerapp/upload/upload.ctrl.js
+++ public/app/modules/photographerapp/upload/upload.ctrl.js
@@ -1,39 +1,40 @@
-angular.module('UploadPageApp')
-    .controller('UploadPageCtrl', [
-        '$scope',
-        function($scope) {
-
-            var units;
-
-            $scope.units = [
-{
-id: 1,
-concatAddr: "2 Huntington",
-unitNum: 10,
-pubid: 1234,
-Floor_Plan: "http://placekitten.com/g/600/400"
-},
-{id: 2,
-concatAddr: "130 Maple Ave",
-unitNum: 2,
-pubid: 12345,
-Floor_Plan: "http://placekitten.com/g/400/600"
-}
-];
-
-
-            $("#floorplan").click(function(e){
-                var x = e.pageX - this.offsetLeft;
-                var y = e.pageY - this.offsetTop;
-                console.log("x =" + x);
-                console.log("y =" + y);
-
-            });
-
-
-
-
-
-
+angular.module('UploadPageApp').controller('UploadPageCtrl', [
+    '$scope',
+    '$resource',
+    'WizioConfig',
+    function($scope, $resource, WizioConfig) {
+        var units;
+        var selectedUnit;
+        //
+        // $resource(WizioConfig.baseAPIURL + '/apartment/uploadtool').get(function(response) {
+        // });
+        var response = [
+            {
+                id: 1,
+                concatAddr: "2 Huntington",
+                unitNum: 10,
+                pubid: 1234,
+                Floor_Plan: "http://placekitten.com/g/600/400"
+            }, {
+                id: 2,
+                concatAddr: "130 Maple Ave",
+                unitNum: 2,
+                pubid: 12345,
+                Floor_Plan: "http://placekitten.com/g/400/600"
+            }
+        ];
+        $scope.units = response;
+        $scope.loadFloorplan = function(subScope){
+            $scope.selectedFloorplan = subScope.unit.Floor_Plan;
+            selectedUnit = subScope.unit
         }
-    ]);
+        document.getElementById('floorplan').addEventListener('click', function(e){
+            console.dir(e);
+            var x = e.pageX - this.offsetLeft;
+            var y = e.pageY - this.offsetTop;
+            console.log("x =" + ((x/e.srcElement.clientWidth)*100).toFixed(2));
+            console.log("y =" + ((y/e.srcElement.clientHeight)*100).toFixed(2));
+        })
+
+    }
+]);
