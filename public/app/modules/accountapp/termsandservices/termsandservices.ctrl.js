29f59a6cf0b00ebf05fc1fffb12e51596be95dee
Cameron <cameron.billings92@gmail.com>
2016-11-15 09:17:14 -0500

    continue button fixed

--- public/app/modules/accountapp/termsandservices/termsandservices.ctrl.js
+++ public/app/modules/accountapp/termsandservices/termsandservices.ctrl.js
@@ -1,15 +1,25 @@
 angular.module('AccountApp')
     .controller('TermsAndServicesCtrl', [
         '$scope',
-        function($scope){
+        '$uibModalInstance',
+        function($scope, $uibModalInstance) {
+            // flag for displaying continue button on modal
+            $scope.pass = false;
 
-        $scope.pass = false;
+            // Each time a check box is clicked, run this function
+            $scope.showContinue = function() {
+                var termsChkBx = document.getElementById('terms').checked;
+                var nextChkBx = document.getElementById('next').checked;
+                var lastChkBx = document.getElementById('last').checked;
+                var continueButton = document.getElementById('contbutton');
+                if (termsChkBx && nextChkBx && lastChkBx) {
 
-        $scope.showContinue = function() {
+                    continueButton.style.visibility = "visible";
+                }
+            };
 
-            if (document.getElementById('terms').checked && document.getElementById('next').checked && document.getElementById('last').checked) {
-                document.getElementById('contbutton').style.visibility = "visible";
+            $scope.ok = function() {
+                $uibModalInstance.close('success');
             }
-        };
-    }
+        }
     ])
