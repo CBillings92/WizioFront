e171928a63fbec0347ba0f6ee7725464e504f38d
U-DESKTOP-ENM503I\Camer <cameron.billings92@gmail.com>
2016-11-14 23:15:11 -0500

    new floorplan and vr player html

--- public/app/modules/accountapp/termsandservices/termsandservices.ctrl.js
+++ public/app/modules/accountapp/termsandservices/termsandservices.ctrl.js
@@ -2,12 +2,21 @@ angular.module('AccountApp')
     .controller('TermsAndServicesCtrl', [
         '$scope',
         function($scope){
+        var getElemByID = document.getElementById;
+
 
         $scope.pass = false;
 
         $scope.showContinue = function() {
 
-            if (document.getElementById('terms').checked && document.getElementById('next').checked && document.getElementById('last').checked) {
+            var termsChecked = getElemByID('terms').checked;
+            var nextChecked = getElemByID('next').checked;
+            var lastChecked = getElemByID('last').checked;
+            if(
+                document.getElementById('terms').checked
+                && document.getElementById('next').checked
+                && document.getElementById('last').checked
+            ) {
                 document.getElementById('contbutton').style.visibility = "visible";
             }
         };
