6c63a8aed2a594698d8443887698dca147cb36b6
U-DESKTOP-ENM503I\Camer <cameron.billings92@gmail.com>
2016-11-27 12:41:35 -0500

    signup form changes forgot to commit_

--- public/app/modules/accountapp/auth2app/controllers/signupform.ctrl.js
+++ public/app/modules/accountapp/auth2app/controllers/signupform.ctrl.js
@@ -25,7 +25,9 @@ angular.module('AccountApp')
                 image: 'https://s3.amazonaws.com/stripe-uploads/acct_16XvxPDqEKTsTxvomerchant-icon-1471400059126-Untitled-1.png',
                 locale: 'auto',
                 token: function stripecb(token) {
+                    console.dir('hi');
                     stripetoken = token;
+                    console.dir(token);
                     stripetoken.userid = TokenSvc.decode().id;
                     stripetoken.user = TokenSvc.decode();
                     $resource(WizioConfig.baseAPIURL + "user/subscribe")
