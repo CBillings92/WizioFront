74f4113f29d849893435f2e112b66d569a40d3c0
U-DESKTOP-ENM503I\Camer <cameron.billings92@gmail.com>
2016-12-27 16:59:39 -0500

    typeahead on search

--- public/app/modules/searchapp/main/search.ctrl.js
+++ public/app/modules/searchapp/main/search.ctrl.js
@@ -1,4 +1,16 @@
 angular.module('SearchApp')
-    .controller('SearchCtrl', ['$scope', function($scope) {
+    .controller('SearchCtrl', [
+        '$scope',
+        'SmartSearchSvc',
+        function($scope, SmartSearchSvc) {
+        // ADD TYPE AHEAD CODE
+        //smart search/typeahead functionality
+        $scope.getLocation = function(val) {
+            return SmartSearchSvc.smartSearch(val);
+        };
 
+        // ADD SUBMIT SEARCH ADDRESS function
+        $scope.submitSearch = function() {
+
+        }
     }]);
