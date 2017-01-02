380de5e0490c82e805e29d3aa0ef9ccf2e1e2c25
Cameron Billings <Cameron.billings92@gmail.com>
2017-01-02 13:21:25 -0500

    added clipboard object to activelistings controller for copy link

--- public/app/modules/activelistingsapp/list/activelisting.list.ctrl.js
+++ public/app/modules/activelistingsapp/list/activelisting.list.ctrl.js
@@ -1,4 +1,5 @@
 angular.module('AccountApp')
     .controller('ActiveListingListCtrl', ['$scope', function($scope) {
+        new Clipboard('.clipboard');
         $scope.windowLocationOrigin = window.location.origin;
     }]);
