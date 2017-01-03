f9f1a916dc8935b3793c4715fe2a81297b75f9d5
Cameron Billings <Cameron.billings92@gmail.com>
2017-01-03 11:36:00 -0500

    added open tour button

--- public/app/modules/activelistingsapp/list/activelisting.list.ctrl.js
+++ public/app/modules/activelistingsapp/list/activelisting.list.ctrl.js
@@ -2,4 +2,7 @@ angular.module('AccountApp')
     .controller('ActiveListingListCtrl', ['$scope', function($scope) {
         new Clipboard('.clipboard');
         $scope.windowLocationOrigin = window.location.origin;
+        $scope.openInNewPage = function(pubid){
+          window.open($scope.windowLocationOrigin + '/tour/' + pubid, '_blank');
+        }
     }]);
