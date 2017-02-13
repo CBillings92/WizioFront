65c0e7ca4c2550bc66b6448f71377a3716dbcdeb
trentduffy <ct_trentduffy@me.com>
2017-02-12 22:52:30 -0800

    agent profile frontend done and account page started

--- public/app/modules/agent-profile-app/agent-profile.ctrl.js
+++ public/app/modules/agent-profile-app/agent-profile.ctrl.js
@@ -1,21 +1,44 @@
 angular.module('AgentProfileApp')
 .controller('AgentProfileCtrl', ['$scope', function($scope) {
-    $.fn.flyout = function(options) {
-       var el = this;
-       var edge = options.edge || 30;
-       el.css({
-           position : "absolute",
-           top : "30px",
-           right : -1 * (el.width() - edge)
-       });
-       el.click(function () {
-           $(this).animate({right : "0px"});
-       });
-       el.mouseleave(function () {
-           $(this).animate({right : -1 * (el.width() - edge) });
-       });
+
+    // var out = false;
+    $scope.animation_var = "animation-start";
+
+
+    $scope.toggleFlyout = function() {
+
+    if ($scope.animation_var === "animation-start")
+        $scope.animation_var = "animation-end";
+    else
+        $scope.animation_var = "animation-start";
+
+
     }
-       $("#flyout").flyout({"edge" : "30"});
+
+
+    // $.fn.flyout = function(options) {
+    //    var el = this;
+    //    var edge = options.edge || 30;
+    //    el.css({
+    //        position : "absolute",
+    //        top : "30px",
+    //        right : -1 * (el.width() - edge)
+    //    });
+    //    el.click(function () {
+    //        if (out) {
+    //            $(this).animate({right : -1 * (el.width() - edge) });
+    //        } else {
+    //        $(this).animate({right : "0px"});
+    //     }
+    //     out = !out;
+    //
+    //    });
+    //    el.mouseleave(function () {
+    //        $(this).animate({right : -1 * (el.width() - edge) });
+    //
+    //    });
+    // }
+    //    $("#flyout").flyout({"edge" : "30"});
 
 
 
