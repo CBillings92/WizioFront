b6c69a8ebdae31e642104d9642970d9e71eebd36
CBillings92 <Cameron.billings92@gmail.com>
2017-06-14 21:24:24 -0400

    loading image icon and design updates for invite user

--- public/app/modules/dashboard-app/invite/invite.ctrl.js
+++ public/app/modules/dashboard-app/invite/invite.ctrl.js
@@ -3,11 +3,21 @@ angular.module('DashboardApp')
         '$scope',
         'InviteFct',
         '$state',
-        function ($scope, InviteFct, $state) {
+        'LoadingSpinnerFct',
+        function ($scope, InviteFct, $state, LoadingSpinnerFct) {
+            $scope.SubmitButtonConfig = {
+                text: 'Send Invite',
+                disabled: false
+            };
             $scope.inviteUser = function () {
+                $scope.SubmitButtonConfig.text = 'Sending Invite...';
+                $scope.SubmitButtonConfig.disabled = true;
+                LoadingSpinnerFct.show('dashboard-invite-user-spinner');
                 InviteFct.inviteUser($scope.emailOfInvitee)
                 .then(function(response){
-
+                    $scope.SubmitButtonConfig.text = 'Send Invite';
+                    $scope.SubmitButtonConfig.disabled = false;
+                    LoadingSpinnerFct.hide('dashboard-invite-user-spinner');
                 });
             }
         }
