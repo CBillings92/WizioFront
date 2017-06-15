17d800150977fced417aff4344fa6918097fe3c4
CBillings92 <Cameron.billings92@gmail.com>
2017-06-15 00:56:27 -0400

    reset password fix

--- public/app/modules/change-password-app/change-password-email-form/change-password-email-form.ctrl.js
+++ public/app/modules/change-password-app/change-password-email-form/change-password-email-form.ctrl.js
@@ -1,8 +1,11 @@
 angular.module('AccountApp')
-    .controller('ChangePasswordEmailFormCtrl', ['$scope', 'ChangePasswordFct', function($scope, ChangePasswordFct) {
+    .controller('ChangePasswordEmailFormCtrl', ['$scope', 'ChangePasswordFct', 'LoadingSpinnerFct', function($scope, ChangePasswordFct, LoadingSpinnerFct) {
         $scope.forgotPassword = function(){
+            LoadingSpinnerFct.show('send-reset-password-email');
             ChangePasswordFct.api.get.email($scope.passwordResetEmail)
             .then(function(apiResponse){
+                LoadingSpinnerFct.hide('send-reset-password-email');
+                alert('Password Reset Email Sent');
 
             })
             .catch(function(err){
