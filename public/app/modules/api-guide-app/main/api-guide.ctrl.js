293f0da5cb833244862e3490bc9909215d1767e2
Cameron <cameron.billings92@gmail.com>
2017-12-04 00:03:22 -0500

    updating api documentation

--- public/app/modules/api-guide-app/main/api-guide.ctrl.js
+++ public/app/modules/api-guide-app/main/api-guide.ctrl.js
@@ -7,19 +7,6 @@ angular.module('ApiGuideApp')
         'ModalBuilderFct',
         'WizioConfig',
         function ($scope, ModalBuilderFct, WizioConfig) {
-          var pagecontainer = document.getElementById('apiguidewrapper')
-          var parser = new DOMParser();
-          var converter = new showdown.Converter()
-          pagecontainer.appendChild(parser.parseFromString(converter.makeHtml('#Wizio API Guide'), 'text/xml').firstChild);
-
-          console.dir(document.getElementById('test'));
-          console.dir(converter.makeHtml(document.getElementById('test').value))
-          console.dir(document.getElementById('test2').innerHtml)
-          console.dir(document.getElementById('test2'))
-          setTimeout(function(){
-
-            document.getElementById('test2').innerHtml = converter.makeHtml(document.getElementById('test').value)
-          }, 1000)
 
             $scope.triggerModal = function () {
                 // api-request configurations to build modal with
