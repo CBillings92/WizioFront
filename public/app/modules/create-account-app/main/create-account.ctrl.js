064ba333182625d454e765e96173f6c9177f70e2
Cameron Billings <Cameron.billings92@gmail.com>
2018-01-18 23:47:10 -0500

    Release candidate (#551)
    * clicking acct btn does not hide account data anymore (#534)
    
    * added terms and conditions app/page onto site with new terms (#533)
    
    * Support modal (#532)
    
    * Modal 1- intro to page
    
    * modal styling
    
    * all modals in place
    
    * cleaned up support modal
    
    * fixing some spacing issues on mobile
    
    * Create a separate scss file
    
    * remove inline styling
    
    * Support modal (#535)
    
    * Modal 1- intro to page
    
    * modal styling
    
    * all modals in place
    
    * cleaned up support modal
    
    * fixing some spacing issues on mobile
    
    * Create a separate scss file
    
    * remove inline styling
    
    * added support-modal to import sass page
    
    * updating terms of service link on signup page
    
    * Iframe btn (#536)
    
    * added copy iframe button
    
    * copy iframe button
    
    * Support modal (#537)
    
    * Modal 1- intro to page
    
    * modal styling
    
    * all modals in place
    
    * cleaned up support modal
    
    * fixing some spacing issues on mobile
    
    * Create a separate scss file
    
    * remove inline styling
    
    * added support-modal to import sass page
    
    * close button adjust, update second gif
    
    * spacing around close button
    
    * reduced size of gif
    
    * increasing size of gif
    
    * Empty state directive (#538)
    
    * added directive for empty state
    
    * removing test dir html from dashboard menu
    
    * privacypolicy (#539)
    
    * fix js, add in empty background and logo (#540)
    
    * rm consol logs, add privacy policy, fix $scope variable bug
    
    * fixed design bugs with modals
    
    * fixed typo bug added variable back to scope (#542)
    
    * dockerized wiziofront (#544)
    
    * Privacy Policy and Terms of Service copy update (#545)
    
    * quick change per devon
    
    * Pricing page updates (#546)
    
    * FAQ copy updates
    
    * update content, rough layout
    
    * update copy and establish design on desktop-lg
    
    * add lines to pricing block, partner logos
    
    * mobile sizing
    
    * fixed png routing
    
    * updatingapp version num
    
    * fixed gulp file
    
    * adding hard coded airbnb contact info (#548)
    
    * fixed package.json file for aws
    
    * updating app version
    
    * ran build for picking up changes to contact info
    
    * fixed registration bug (#550)
    
    * fixed registration bug
    
    * removing console log

--- public/app/modules/create-account-app/main/create-account.ctrl.js
+++ public/app/modules/create-account-app/main/create-account.ctrl.js
@@ -1,78 +1,81 @@
-angular.module('CreateAccountApp')
-    .controller('CreateAccountCtrl', [
-        '$scope',
-        '$state',
-        'CreateAccountFct',
-        '$window',
-        'WizioConfig',
-        'TokenSvc',
-        '$resource',
-        'ModalBuilderFct',
-        function($scope, $state, CreateAccountFct, $window, WizioConfig, TokenSvc, $resource, ModalBuilderFct) {
-            //
-            $scope.numUsersNotNeeded = true;
-            $scope.signupInviteFlag = $state.current.name === "Signup.Invite" ? true : false;
+angular.module("CreateAccountApp").controller("CreateAccountCtrl", [
+  "$scope",
+  "$state",
+  "CreateAccountFct",
+  "$window",
+  "WizioConfig",
+  "TokenSvc",
+  "$resource",
+  "ModalBuilderFct",
+  function($scope, $state, CreateAccountFct, $window, WizioConfig, TokenSvc, $resource, ModalBuilderFct) {
+    //
+    $scope.numUsersNotNeeded = true;
+    $scope.signupInviteFlag = $state.current.name === "Signup.Invite" ? true : false;
 
-            $scope.submit = function() {
-                var subscription;
-                var user = $scope.user;
-                if (user.password != user.passwordConfirm) {
-                    ModalBuilderFct.buildSimpleModal("", "OK", "Error", 'Your passwords do not match!').then(function(result) {
-                        return;
-                    });
-                    return;
-                }
-                if($scope.signupInviteFlag === false) {
-                    if (user.code != "931422") {
-                        ModalBuilderFct.buildSimpleModal("", "OK", "Error", 'You do not have the correct signup code!').then(function(result) {
-                            return;
-                        });
-                        return;
-                    }
-                }
-
-
-                // Check if the user was invited or if it's a new registration
-                if ($scope.signupInviteFlag) {
-                    user.invitePubId = $state.params.invitePubId;
-                } else {
-                    subscription = $scope.chosenSubscription;
-                }
-
-
-
-                CreateAccountFct.post.saveNewUser(user, subscription).then(function(response) {
-                    $state.go('Account.Dashboard');
-                });
-            };
-            $scope.getPlan = function() {
-                return $scope.chosenSubscription = '2';
-            }
-
-
-            var stripetoken;
-            var handler = $window.StripeCheckout.configure({
-                key: WizioConfig.stripe_test_key,
-                image: 'https://s3.amazonaws.com/stripe-uploads/acct_16XvxPDqEKTsTxvomerchant-icon-1471400059126-Untitled-1.png',
-                locale: 'auto',
-                token: function stripecb(token) {
-                    stripetoken = token;
-                    stripetoken.userid = TokenSvc.decode().id;
-                    stripetoken.user = TokenSvc.decode();
-                    stripetoken.plan_id = $scope.chosenSubscription;
-                    stripetoken.total_users = 1;
-                    $resource(WizioConfig.baseAPIURL + "user/subscribe")
-                    .save(stripetoken, function(response) {
-                        return response;
-                    });
-                }
-            });
-            //
-            // handler.open({
-            //     name: 'Wizio Inc.,',
-            //     description: 'Subscribe below:',
-            //     zipCode: true,
-            //     // email: $scope.user.email,
-            // });
+    $scope.submit = function() {
+      var subscription;
+      var user = $scope.user;
+      if (user.password != user.passwordConfirm) {
+        ModalBuilderFct.buildSimpleModal("", "OK", "Error", "Your passwords do not match!").then(function(result) {
+          return;
+        });
+        return;
+      }
+      if ($scope.signupInviteFlag === false) {
+        if (user.code != "931422") {
+          ModalBuilderFct.buildSimpleModal("", "OK", "Error", "You do not have the correct signup code!").then(function(
+            result
+          ) {
+            return;
+          });
+          return;
         }
-    ]);
+      }
+
+      // Check if the user was invited or if it's a new registration
+      if ($scope.signupInviteFlag) {
+        user.invitePubId = $state.params.invitePubId;
+      } else {
+        subscription = $scope.chosenSubscription;
+      }
+
+      CreateAccountFct.post
+        .saveNewUser(user, subscription)
+        .then(function(response) {
+          $state.go("Account.Dashboard");
+        })
+        .catch(function(errMessage) {
+          ModalBuilderFct.buildSimpleModal("", "OK", "Error", errMessage).then(function(result) {
+            return;
+          });
+        });
+    };
+    $scope.getPlan = function() {
+      return ($scope.chosenSubscription = "2");
+    };
+
+    var stripetoken;
+    var handler = $window.StripeCheckout.configure({
+      key: WizioConfig.stripe_test_key,
+      image: "https://s3.amazonaws.com/stripe-uploads/acct_16XvxPDqEKTsTxvomerchant-icon-1471400059126-Untitled-1.png",
+      locale: "auto",
+      token: function stripecb(token) {
+        stripetoken = token;
+        stripetoken.userid = TokenSvc.decode().id;
+        stripetoken.user = TokenSvc.decode();
+        stripetoken.plan_id = $scope.chosenSubscription;
+        stripetoken.total_users = 1;
+        $resource(WizioConfig.baseAPIURL + "user/subscribe").save(stripetoken, function(response) {
+          return response;
+        });
+      }
+    });
+    //
+    // handler.open({
+    //     name: 'Wizio Inc.,',
+    //     description: 'Subscribe below:',
+    //     zipCode: true,
+    //     // email: $scope.user.email,
+    // });
+  }
+]);
