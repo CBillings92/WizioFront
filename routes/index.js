36845efac5d20ffecc7103d83cfb824a84ccd6a7
Cameron <cameron.billings92@gmail.com>
2018-01-21 21:42:39 -0500

    trial 26

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/tour/*', function (req, res, next ) {
 router.get('/', function(req, res, next) {
     res.render('index', {
         title: 'Express',
-        ogImageContent: 'https://cdn.wizio.co/cb029dc4-15ce-4d97-96fd-f8c8c84aba15/Living%20Room%201.JPG'
+        ogImageContent: 'https://cdn.wizio.co/cb029dc4-15ce-4d97-96fd-f8c8c84aba15/Living%20Room%201.JPG',
         ogUrl: 'http://alpha.wizio.co/'
     });
 });
