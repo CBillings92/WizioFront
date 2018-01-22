6817d221328326969008fb7c2d63f09ff18ab811
Cameron <cameron.billings92@gmail.com>
2018-01-22 09:44:56 -0500

    got it working

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,42 @@
-var express = require('express');
+var express = require("express");
 // var config = require('../config/config.js');
 var router = express.Router();
+var request = require("request");
 
-router.get('/tour/*', function (req, res, next ) {
-  console.dir(req);
-  res.render('index', {
-    ogImageContent: 'https://cdn.wizio.co/1000x500/cb029dc4-15ce-4d97-96fd-f8c8c84aba15/Kitchen.JPG',
-    ogUrl: 'http://alpha.wizio.co/tour/0994f2d2-0310-41e0-8036-329fb3e14856'
-  })
-});
+router.get("/tour/:tourid", function(req, res, next) {
+  console.dir("FUCK");
+  var url = "http://127.0.0.1:4000/api/activelisting/" + req.params.tourid;
+  console.dir(url);
 
-router.get('/', function(req, res, next) {
-    res.render('index', {
-        title: 'Express',
-        ogImageContent: 'https://cdn.wizio.co/cb029dc4-15ce-4d97-96fd-f8c8c84aba15/Living%20Room%201.JPG',
-        ogUrl: 'http://alpha.wizio.co/'
+  request("http://172.18.0.2:4000/api/activelisting/" + req.params.tourid, function(err, apires, body) {
+    var url =
+      "https://cdn.wizio.co/" +
+      JSON.parse(body)[0].SubscriptionAptMedia.pubid +
+      "/" +
+      encodeURIComponent(JSON.parse(body)[0].title) +
+      ".JPG";
+    res.render("index", {
+      ogImageContent: url,
+      ogUrl: "http://alpha.wizio.co/tour/0994f2d2-0310-41e0-8036-329fb3e14856"
     });
+  });
 });
 
-
-router.get('/test', function(req, res, next) {
-    res.render('sdfa', {
-        title: 'Express'
-    });
+router.get("/", function(req, res, next) {
+  res.render("index", {
+    title: "Express",
+    ogImageContent: "https://cdn.wizio.co/cb029dc4-15ce-4d97-96fd-f8c8c84aba15/Living%20Room%201.JPG",
+    ogUrl: "http://alpha.wizio.co/"
+  });
 });
-router.get('/envbeurl', function(req, res, next){
-    // res.send(config.backendAPIURL);
+
+router.get("/test", function(req, res, next) {
+  res.render("sdfa", {
+    title: "Express"
+  });
+});
+router.get("/envbeurl", function(req, res, next) {
+  // res.send(config.backendAPIURL);
 });
 
 module.exports = router;
