368c0896d929d4b4e14b39805eac3be2f056e30c
Cameron Billings <Cameron.billings92@gmail.com>
2018-01-29 20:54:24 -0500

    Listing page (#555)
    * bad practice outline
    
    * merge in paths, change amenities from arrays, re org listing page files
    
    * all data in, styling
    
    * demo listing page
    
    * changing up listing description
    
    * fixed bed bath count updated app version num
    
    * updated app version and agent name and realty on listing page
    
    * some kind of change
    
    * renaming product page name to proper name
    
    * updating package version

--- public/app/modules/listing-page-app/listing-page.ctrl.js
+++ public/app/modules/listing-page-app/listing-page.ctrl.js
@@ -8,31 +8,50 @@ angular.module("ListingPageApp").controller("ListingPageCtrl", [
       Listing: {
         concatAddr: "228 South Street Jamaica Plain MA 02130",
         unitNum: "#2",
-        description: "This is a description of the apartment!",
-        amenities: [
-          {
-            WasherDryer: 1,
+        neighborhood: "Back Bay",
+        amenitiesDescription:
+          "Pet-Friendly, Balcony, Fireplace, Fitness center, Roof Deck, 24 Hour security, remote door bell, maintence team on site",
+        amenities: {
+          WasherDryer: {
             Description: "In Unit"
           },
-          {
-            Pool: 1
+          DishWasher: true,
+          Pool: {
+            Description: ""
+          },
+          Gym: false,
+          SecureEntrance: {
+            Description: ""
+          },
+          PetFriendly: {
+            Cats: true,
+            Dogs: true
           }
-        ],
+        },
+        Bedrooms: "3 Bedrooms",
+        Bathrooms: "1.5 Bathrooms",
         rent: "2100",
+        availability: "2.17.18",
         rentFrequency: "per month",
         leaseType: "Annual",
         securityDeposit: {
-          required: 1,
+          required: true,
           total: "2100"
         },
         firstMonthRent: {
-          required: 1,
+          required: true,
           total: "2100"
         },
         lastMonthRent: {
-          required: 0,
+          required: true,
           total: "2100"
-        }
+        },
+        brokerFee: {
+          required: false,
+          total: "2100"
+        },
+        ListingDetails:
+          "Fantastic loft available in Downtown Lowell within a 10-15 minute walk to the commuter rail station! Hop on the T and get to Boston's North Station in 45 minutes. If you drive then it's really close to the Lowell Connector where you can get on 495 and over to Chelmsford, Methuen, or Lawrence very quickly. You are also within walking distance to all the restaurants, conveniences, and nightlife of the downtown! This is a TRUE loft converted from a warehouse."
       }
     };
   }
