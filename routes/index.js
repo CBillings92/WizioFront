98c506659082b0ba0c372e8a04749100f8c3c175
Cameron <cameron.billings92@gmail.com>
2018-02-14 09:33:16 -0500

    listing sahring as well

--- routes/index.js
+++ routes/index.js
@@ -5,30 +5,24 @@ var request = require("request");
 var config = require("../config");
 
 router.get("/tour/:tourid", function(req, res, next) {
-  console.dir("FUCK");
-  console.dir(config.backendAPIURL);
-  console.dir(req.params);
-  console.dir(request);
   request(config.backendAPIURL + "activelisting/" + req.params.tourid, function(err, apires, body) {
     var url = "https://cdn.wizio.co/cb029dc4-15ce-4d97-96fd-f8c8c84aba15/Living%20Room%201.JPG";
-    console.dir("FUCK");
-    console.dir(err);
-    console.dir(apires);
-    console.dir(body);
+    var description = "360 Virtual Tour";
     if (body) {
       try {
         url =
           config.s3bucketURL +
           "/1000x500/" +
-          JSON.parse(body)[0].SubscriptionApartmentPubId +
+          JSON.parse(body).media[0].SubscriptionApartmentPubId +
           "/" +
-          encodeURIComponent(JSON.parse(body)[0].title) +
+          encodeURIComponent(JSON.parse(body).media[0].title) +
           ".JPG";
       } catch (e) {
         console.dir(e);
       } finally {
         res.render("index", {
           ogImageContent: url,
+          ogDescription: description,
           ogUrl: config.frontendURL + "/tour/" + req.params.tourid
         });
       }
@@ -44,6 +38,33 @@ router.get("/listing/a3885803-1100-450f-931d-fbb53b6ed410", function(req, res, n
       "Fantastic 2 bed 1 bath available in Mission Hill. The unit is easily accessible by the MBTA - just a 10 minute walk to the Green Line E train or a few minutes walk to the 14 Bus Route! Hop on the T and get to downtown Boston in 45 minutes. Local night life, restaurants, super markets and convenience stores can also be found within a quick 10 minute walk. Unit is incredibly spacious with an additional basement room that can be used for storage or as an office. Split level offers unique look and feel. This is an unbeatable price for the size and location of this unit!",
     ogUrl: "https://www.wizio.co/listing/a3885803-1100-450f-931d-fbb53b6ed410",
     ogTitle: "2 Bed 1 Bath in Mission Hill - $2700"
+  });
+});
+router.get("/listing/:tourid", function(req, res, next) {
+  request(config.backendAPIURL + "activelisting/" + req.params.tourid, function(err, apires, body) {
+    var url = "https://cdn.wizio.co/cb029dc4-15ce-4d97-96fd-f8c8c84aba15/Living%20Room%201.JPG";
+    var description = "360 Virtual Tour";
+    if (body) {
+      try {
+        url =
+          config.s3bucketURL +
+          "/1000x500/" +
+          JSON.parse(body).media[0].SubscriptionApartmentPubId +
+          "/" +
+          encodeURIComponent(JSON.parse(body).media[0].title) +
+          ".JPG";
+
+        description = JSON.parse(body).Listing.Description;
+      } catch (e) {
+        console.dir(e);
+      } finally {
+        res.render("index", {
+          ogImageContent: url,
+          ogDescription: description,
+          ogUrl: config.frontendURL + "/tour/" + req.params.tourid
+        });
+      }
+    }
   });
 });
 
