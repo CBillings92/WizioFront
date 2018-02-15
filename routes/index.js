8d2d850837bf3b239ef2ffda24a0153338a1c261
Cameron <cameron.billings92@gmail.com>
2018-02-15 06:47:27 -0500

    social sharing

--- routes/index.js
+++ routes/index.js
@@ -4,31 +4,45 @@ var router = express.Router();
 var request = require("request");
 var config = require("../config");
 
-// router.get("/tour/:tourid", function(req, res, next) {
-//   request(config.backendAPIURL + "activelisting/" + req.params.tourid, function(err, apires, body) {
-//     var url = "https://cdn.wizio.co/cb029dc4-15ce-4d97-96fd-f8c8c84aba15/Living%20Room%201.JPG";
-//     var description = "360 Virtual Tour";
-//     if (body) {
-//       try {
-//         url =
-//           config.s3bucketURL +
-//           "/1000x500/" +
-//           JSON.parse(body).media[0].SubscriptionApartmentPubId +
-//           "/" +
-//           encodeURIComponent(JSON.parse(body).media[0].title) +
-//           ".JPG";
-//       } catch (e) {
-//         console.dir(e);
-//       } finally {
-//         res.render("index", {
-//           ogImageContent: url,
-//           ogDescription: description,
-//           ogUrl: config.frontendURL + "/tour/" + req.params.tourid
-//         });
-//       }
-//     }
-//   });
-// });
+router.get("/tour/:tourid", function(req, res, next) {
+  request(config.backendAPIURL + "activelisting/" + req.params.tourid, function(err, apires, body) {
+    var url = "https://cdn.wizio.co/cb029dc4-15ce-4d97-96fd-f8c8c84aba15/Living%20Room%201.JPG";
+    var description = "";
+    var title = "360 Virtual Tour";
+    var neighborhood = "";
+    var locality = "";
+    if (body) {
+      try {
+        var body = JSON.parse(body);
+        url =
+          config.s3bucketURL +
+          "/1000x500/" +
+          body.media[0].SubscriptionApartmentPubId +
+          "/" +
+          encodeURIComponent(body.media[0].title) +
+          ".JPG";
+        description = body.Listing.Description;
+        title = body.Listing.Beds + " bed, " + body.Listing.Baths + " bath unit";
+        if (body.Apartment.neighborhood && body.Apartment.neighborhood !== "") {
+          title = title + " in " + body.Apartment.neighborhood;
+        }
+        if (body.Apartment.locality && body.Apartment.locality !== "") {
+          title = title + ", " + body.Apartment.locality;
+        }
+      } catch (e) {
+        console.dir(e);
+      } finally {
+        res.render("index", {
+          title: "Express",
+          ogTitle: title,
+          ogImageContent: url,
+          ogDescription: description,
+          ogUrl: config.frontendURL + "/tour/" + req.params.tourid
+        });
+      }
+    }
+  });
+});
 
 router.get("/listing/a3885803-1100-450f-931d-fbb53b6ed410", function(req, res, next) {
   res.render("index", {
@@ -40,33 +54,47 @@ router.get("/listing/a3885803-1100-450f-931d-fbb53b6ed410", function(req, res, n
     ogTitle: "2 Bed 1 Bath in Mission Hill - $2700"
   });
 });
-// router.get("/listing/:tourid", function(req, res, next) {
-//   request(config.backendAPIURL + "activelisting/" + req.params.tourid, function(err, apires, body) {
-//     var url = "https://cdn.wizio.co/cb029dc4-15ce-4d97-96fd-f8c8c84aba15/Living%20Room%201.JPG";
-//     var description = "360 Virtual Tour";
-//     if (body) {
-//       try {
-//         url =
-//           config.s3bucketURL +
-//           "/1000x500/" +
-//           JSON.parse(body).media[0].SubscriptionApartmentPubId +
-//           "/" +
-//           encodeURIComponent(JSON.parse(body).media[0].title) +
-//           ".JPG";
-//
-//         description = JSON.parse(body).Listing.Description;
-//       } catch (e) {
-//         console.dir(e);
-//       } finally {
-//         res.render("index", {
-//           ogImageContent: url,
-//           ogDescription: description,
-//           ogUrl: config.frontendURL + "/tour/" + req.params.tourid
-//         });
-//       }
-//     }
-//   });
-// });
+
+router.get("/tour/:tourid", function(req, res, next) {
+  request(config.backendAPIURL + "activelisting/" + req.params.tourid, function(err, apires, body) {
+    var url = "https://cdn.wizio.co/cb029dc4-15ce-4d97-96fd-f8c8c84aba15/Living%20Room%201.JPG";
+    var description = "";
+    var title = "360 Virtual Tour";
+    var neighborhood = "";
+    var locality = "";
+    if (body) {
+      try {
+        var body = JSON.parse(body);
+        url =
+          config.s3bucketURL +
+          "/1000x500/" +
+          body.media[0].SubscriptionApartmentPubId +
+          "/" +
+          encodeURIComponent(body.media[0].title) +
+          ".JPG";
+        description = body.Listing.Description;
+        title = body.Listing.Beds + " bed, " + body.Listing.Baths + " bath unit";
+        if (body.Apartment.neighborhood && body.Apartment.neighborhood !== "") {
+          title = title + " in " + body.Apartment.neighborhood;
+        }
+        if (body.Apartment.locality && body.Apartment.locality !== "") {
+          title = title + ", " + body.Apartment.locality;
+        }
+      } catch (e) {
+        console.dir(e);
+      } finally {
+        console.dir(title);
+        res.render("index", {
+          title: "Express",
+          ogTitle: title,
+          ogImageContent: url,
+          ogDescription: description,
+          ogUrl: config.frontendURL + "/tour/" + req.params.tourid
+        });
+      }
+    }
+  });
+});
 
 router.get("/*", function(req, res, next) {
   res.render("index", {
