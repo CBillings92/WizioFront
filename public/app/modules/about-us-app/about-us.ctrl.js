42d8f0bb9c75d54ebf3bb5c88ca8598cd8f22099
Cameron <cameron.billings92@gmail.com>
2018-03-08 08:46:36 -0500

    removed unnecessary photos and fixed path bug

--- public/app/modules/about-us-app/about-us.ctrl.js
+++ public/app/modules/about-us-app/about-us.ctrl.js
@@ -1,41 +1,42 @@
 /*
     Controller for the About Us page (www.wizio.co/about)
 */
-angular.module('AboutUsApp')
-    .controller('AboutUsCtrl', [
-        '$scope',
-        function ($scope) {
-            $scope.members =
-            [{
-                name: "Cameron Billings",
-                title: "CTO",
-                picture: "Cameron.jpg",
-                twitter: "CamBillings_",
-                linkedin: "cameronbillings",
-                email: "cameron@wizio.co",
-            }, {
-                name: "Devon Grodkiewicz",
-                title: "CEO",
-                picture: "Devon.jpg",
-                twitter: "dsgrod",
-                linkedin: "dsgrod",
-                email: "devon@wizio.co",
-            }, {
-                name: "John Puma",
-                title: "COO",
-                picture: "John.jpg",
-                twitter: "JohnJPuma",
-                linkedin: "johnjpuma",
-                email: "john@wizio.co",
-            }, {
-                name: "Emily O'Brien",
-                title: "Director of Product Design",
-                picture: "emily.jpg",
-                twitter: "",
-                linkedin: "emilymobrien4",
-                email: "emily@wizio.co",
-            }];
-        }
-
-  
-    ]);
+angular.module("AboutUsApp").controller("AboutUsCtrl", [
+  "$scope",
+  function($scope) {
+    $scope.members = [
+      {
+        name: "Cameron Billings",
+        title: "CTO",
+        picture: "Cameron.jpg",
+        twitter: "CamBillings_",
+        linkedin: "cameronbillings",
+        email: "cameron@wizio.co"
+      },
+      {
+        name: "Devon Grodkiewicz",
+        title: "CEO",
+        picture: "Devon.jpg",
+        twitter: "dsgrod",
+        linkedin: "dsgrod",
+        email: "devon@wizio.co"
+      },
+      {
+        name: "John Puma",
+        title: "COO",
+        picture: "John.jpg",
+        twitter: "JohnJPuma",
+        linkedin: "johnjpuma",
+        email: "john@wizio.co"
+      },
+      {
+        name: "Emily O'Brien",
+        title: "Director of Product Design",
+        picture: "Emily.jpg",
+        twitter: "",
+        linkedin: "emilymobrien4",
+        email: "emily@wizio.co"
+      }
+    ];
+  }
+]);
