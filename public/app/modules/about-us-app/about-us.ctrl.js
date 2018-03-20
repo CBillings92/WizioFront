8cafc40f403fb5a90f3efbb0afbcc091efef6ecd
emobrien28 <emobrien28@gmail.com>
2018-03-20 05:57:37 -0700

    About page (#585)
    * header, stats, team sections
    
    * New copy, contact design
    
    * smallest mobile responsive
    
    * tablet responsive
    
    * removed unnecessary photos and fixed path bug
    
    * darken CTA border, add shadow
    
    * photographer typeform link
    
    * turn off transparent nav
    
    * merge in order to add updates
    
    * mission and top blurb changes

--- public/app/modules/about-us-app/about-us.ctrl.js
+++ public/app/modules/about-us-app/about-us.ctrl.js
@@ -1,53 +1,42 @@
 /*
     Controller for the About Us page (www.wizio.co/about)
 */
-angular.module('AboutUsApp')
-    .controller('AboutUsCtrl', [
-        '$scope',
-        function ($scope) {
-            $scope.members =
-            [{
-                name: "Cameron Billings",
-                title: "CTO",
-                picture: "cameronbillings.png",
-                twitter: "CamBillings_",
-                linkedin: "cameronbillings",
-                email: "cameron@wizio.co",
-            }, {
-                name: "Devon Grodkiewicz",
-                title: "CEO",
-                picture: "devongrodkiewicz.jpg",
-                twitter: "dsgrod",
-                linkedin: "dsgrod",
-                email: "devon@wizio.co",
-            }, {
-                name: "Trent Duffy",
-                title: "Co-Founder and Head of Product",
-                picture: "trentduffy.png",
-                twitter: "trentolol",
-                linkedin: "trentduffy",
-                email: "trent@wizio.co",
-            }, {
-                name: "John Puma",
-                title: "COO",
-                picture: "johnjpuma.jpg",
-                twitter: "JohnJPuma",
-                linkedin: "johnjpuma",
-                email: "john@wizio.co",
-            }, {
-                name: "Emily O'Brien",
-                title: "Director of Product Design",
-                picture: "emily.jpg",
-                twitter: "",
-                linkedin: "emilymobrien4",
-                email: "emily@wizio.co",
-            }, {
-                name: "Yuyang He",
-                title: "Video Production Co-op",
-                picture: "yuyang.JPG",
-                twitter: "",
-                linkedin: "yuyang-he-ab6087a6",
-                email: "yuyang@wizio.co",
-            }];
-        }
-    ]);
+angular.module("AboutUsApp").controller("AboutUsCtrl", [
+  "$scope",
+  function($scope) {
+    $scope.members = [
+      {
+        name: "Cameron Billings",
+        title: "CTO",
+        picture: "Cameron.jpg",
+        twitter: "CamBillings_",
+        linkedin: "cameronbillings",
+        email: "cameron@wizio.co"
+      },
+      {
+        name: "Devon Grodkiewicz",
+        title: "CEO",
+        picture: "Devon.jpg",
+        twitter: "dsgrod",
+        linkedin: "dsgrod",
+        email: "devon@wizio.co"
+      },
+      {
+        name: "John Puma",
+        title: "COO",
+        picture: "John.jpg",
+        twitter: "JohnJPuma",
+        linkedin: "johnjpuma",
+        email: "john@wizio.co"
+      },
+      {
+        name: "Emily O'Brien",
+        title: "Director of Product Design",
+        picture: "Emily.jpg",
+        twitter: "",
+        linkedin: "emilymobrien4",
+        email: "emily@wizio.co"
+      }
+    ];
+  }
+]);
