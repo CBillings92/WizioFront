8809dda2a512f19a88304207b85d63f0646183d6
Cameron Billings <Cameron.billings92@gmail.com>
2018-04-19 12:15:56 -0400

    adding fairfield realty to MLS compliant tours (#641)

--- public/app/modules/vr-player-interface-app/vr-player-interface.ctrl.js
+++ public/app/modules/vr-player-interface-app/vr-player-interface.ctrl.js
@@ -232,7 +232,8 @@ angular.module("TourApp").controller("VrPlayerInterfaceCtrl", [
       $scope.agent.email === "youngone@younghouses.com" ||
       $scope.agent.email === "brian@lmcrealtyboston.com" ||
       $scope.agent.email === "yuyang@wizio.co" ||
-      $scope.agent.BusinessName === "HqO"
+      $scope.agent.BusinessName === "HqO" ||
+      $scope.agent.BusinessName === "Fairfield Realty"
     ) {
       $scope.isMLSListingAccount = true;
     }
