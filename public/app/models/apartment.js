590b2f8969c079a90c3cad099f28bddbc1a54998
Cameron <cameron.billings92@gmail.com>
2018-05-29 10:34:42 -0400

    removing one more dependency from old angular module

--- public/app/models/apartment.js
+++ public/app/models/apartment.js
@@ -1,267 +1,273 @@
-angular.module('Models')
-    .factory('ApartmentModel', [
-        '$sessionStorage',
-        '$resource',
-        '$q',
-        'lodash',
-        'WizioConfig',
-        'UnitCreateSvc',
-        function($sessionStorage, $resource, $q, lodash, WizioConfig, UnitCreateSvc) {
-            function Apartment(id, street, unitNum, neighborhood,
-                locality,
-                administrative_area_level_3,
-                state,
-                zip,
-                beds,
-                baths,
-                livingSpaces,
-                maxResidency,
-                costPerMonth,
-                renovated,
-                pets,
-                ApartmentType,
-                latitude,
-                longitude,
-                concatAddr,
-                leaseType,
-                leaseExpectedEnd,
-                leaseActualEnd,
-                PropertyManagerId,
-                route,
-                laundry,
-                elevator,
-                gym,
-                pool,
-                parking,
-                concierge,
-                airconditioning,
-                roofDeck,
-                superIntendent,
-                balcony,
-                garage,
-                CreatedById,
-                UpdatedById,
-                pubid,
-                ActiveListings
-            ) {
-                this.apartmentData = {
-                    id: id || null,
-                    street: street || null,
-                    unitNum: unitNum || null,
-                    neighborhood: neighborhood || null,
-                    locality: locality || null,
-                    administrative_area_level_3: administrative_area_level_3 || null,
-                    state: state || null,
-                    zip: zip || null,
-                    beds: beds || null,
-                    baths: baths || null,
-                    livingSpaces: livingSpaces || null,
-                    maxResidency: maxResidency || null,
-                    costPerMonth: costPerMonth || null,
-                    renovated: renovated || null,
-                    ApartmentType: ApartmentType || null,
-                    latitude: latitude || null,
-                    longitude: longitude || null,
-                    concatAddr: concatAddr || null,
-                    PropertyManagerId: PropertyManagerId || null,
-                    route: route || null,
-                    laundry: laundry || null,
-                    elevator: elevator,
-                    gym: gym,
-                    pool: pool,
-                    parking: parking,
-                    concierge: concierge,
-                    airconditioning: airconditioning,
-                    roofDeck: roofDeck,
-                    superIntendent: superIntendent,
-                    balcony: balcony,
-                    garage: garage,
-                    CreatedById: CreatedById || null,
-                    UpdatedById: UpdatedById || null,
-                    pubid: pubid,
-                    ActiveListings: ActiveListings
-                };
+angular.module("Models").factory("ApartmentModel", [
+  "$sessionStorage",
+  "$resource",
+  "$q",
+  "lodash",
+  "WizioConfig",
+  function($sessionStorage, $resource, $q, lodash, WizioConfig) {
+    function Apartment(
+      id,
+      street,
+      unitNum,
+      neighborhood,
+      locality,
+      administrative_area_level_3,
+      state,
+      zip,
+      beds,
+      baths,
+      livingSpaces,
+      maxResidency,
+      costPerMonth,
+      renovated,
+      pets,
+      ApartmentType,
+      latitude,
+      longitude,
+      concatAddr,
+      leaseType,
+      leaseExpectedEnd,
+      leaseActualEnd,
+      PropertyManagerId,
+      route,
+      laundry,
+      elevator,
+      gym,
+      pool,
+      parking,
+      concierge,
+      airconditioning,
+      roofDeck,
+      superIntendent,
+      balcony,
+      garage,
+      CreatedById,
+      UpdatedById,
+      pubid,
+      ActiveListings
+    ) {
+      this.apartmentData = {
+        id: id || null,
+        street: street || null,
+        unitNum: unitNum || null,
+        neighborhood: neighborhood || null,
+        locality: locality || null,
+        administrative_area_level_3: administrative_area_level_3 || null,
+        state: state || null,
+        zip: zip || null,
+        beds: beds || null,
+        baths: baths || null,
+        livingSpaces: livingSpaces || null,
+        maxResidency: maxResidency || null,
+        costPerMonth: costPerMonth || null,
+        renovated: renovated || null,
+        ApartmentType: ApartmentType || null,
+        latitude: latitude || null,
+        longitude: longitude || null,
+        concatAddr: concatAddr || null,
+        PropertyManagerId: PropertyManagerId || null,
+        route: route || null,
+        laundry: laundry || null,
+        elevator: elevator,
+        gym: gym,
+        pool: pool,
+        parking: parking,
+        concierge: concierge,
+        airconditioning: airconditioning,
+        roofDeck: roofDeck,
+        superIntendent: superIntendent,
+        balcony: balcony,
+        garage: garage,
+        CreatedById: CreatedById || null,
+        UpdatedById: UpdatedById || null,
+        pubid: pubid,
+        ActiveListings: ActiveListings
+      };
+    }
+    Apartment.prototype.api = function() {
+      var apartment = this;
+      return {
+        create: function(callback) {
+          $resource(WizioConfig.baseAPIURL + "apartment").save(this.data, function(response) {
+            return callback(response);
+          });
+        },
+        update: function(callback) {
+          $resource(WizioConfig.baseAPIURL + "apartment/:id", {
+            id: "@id"
+          }).save(
+            {
+              id: this.id
+            },
+            this.data,
+            function(response) {
+              return callback(response);
             }
-            Apartment.prototype.api = function(){
-                var apartment = this;
-                return {
-                    create: function(callback) {
-                        $resource(WizioConfig.baseAPIURL + 'apartment').save(this.data, function(response) {
-                            return callback(response);
-                        });
-                    },
-                    update: function(callback) {
-                        $resource(WizioConfig.baseAPIURL + 'apartment/:id', {
-                            id: '@id'
-                        }).save({
-                            id: this.id
-                        }, this.data, function(response) {
-                            return callback(response);
-                        });
-                    },
-                    getById: function(callback) {
-                        $resource(WizioConfig.baseAPIURL + 'apartment/:id', {
-                            id: '@id'
-                        }).get({
-                            id: this.id
-                        }, function(response) {
-                            return callback(response);
-                        });
-                    },
-                    findOrCreate: function(action, callback){
-                        $resource(WizioConfig.baseAPIURL + 'apartment/claim/:action', {
-                            action: "@action"
-                        }).save(action, apartment, function(response){
-                            return callback(response);
-                        });
-                    }
-
-                };
-            };
-            Apartment.prototype.getGeocodeData = function(callback){
-                var apartmentData = this.apartmentData;
-                return $q(function(resolve, reject){
-                    UnitCreateSvc.parseGeocodeData(apartmentData.concatAddr, null, function(err, response){
-                        for(var key in response){
-                            if(response[key] === 'Longitude'){
-                                apartment.Longitude = parseFloat(response[key]).toFixed(6);
-                            } else if (response[key] === 'Latitude'){
-                                apartment.Latitude = parseFloat(response[key]).toFixed(6);
-                            } else {
-                                apartmentData[key] = response[key];
-                            }
-                        }
-                        resolve('done');
-                    });
-                });
-
-            };
-            Apartment.prototype.duplicate = function(){
-                var duplicate = {};
-                for(var key in this.apartmentData){
-                    if(this.apartmentData.hasOwnProperty(key)){
-                        var count = 0;
-                        if(key !== 'id'){
-                            duplicate[key] = this.apartmentData[key];
-                        }
-                    }
-                }
-                return duplicate;
-            };
-            Apartment.setDescription = function(){
-                if(arguments.length > 0){
-                    this.apartmentData.Descriptions = null;
-                    this.apartmentData.Descriptions.description = arguments.description;
-                    // this.apartmentData.Descriptions.UserId
-                }
-            };
-            Apartment.prototype.concealAddress = function(){
-                var left = Math.floor((this.apartmentData.concatAddr.charCodeAt(5) /19) + 4);
-                    var right = Math.floor((this.apartmentData.concatAddr.charCodeAt(3) /19) + 4);
-                    var houseNumInt = parseInt((this.apartmentData.concatAddr).replace(/(^\d+)(.+$)/i, '$1'));
-                    var houseNumLow = houseNumInt - left;
-                    if(houseNumInt < 15){
-                        houseNumLow = 1;
-                    }
-                    var houseNumHigh = houseNumInt + right;
-                    var houseNumRange = houseNumLow.toString() + "-" + houseNumHigh.toString();
-                    this.apartmentData.concatAddr = houseNumRange + this.apartmentData.concatAddr.replace(/^\d+/, '');
-            };
-            // Apartment.prototype.api = function() {
-            //     return {
-            //         oneParam: $resource(WizioConfig.baseAPIURL + 'apartment/:id')
-            //     };
-            // };
-            // Apartment.prototype.api = function() {
-            //     return $resource(WizioConfig.baseAPIURL + 'apartment/:id', {
-            //         id: '@id'
-            //     });
-            // };
-            Apartment.api = function() {
-                return $resource(WizioConfig.baseAPIURL + 'apartment/:id', {
-                    id: '@id'
-                });
-            };
-            Apartment.claimApi = function(data, callback) {
-                return $resource(WizioConfig.baseAPIURL + 'apartment/claim/finalize').save(data, function(res){
-                    return callback(res);
-                });
-            };
-
-            Apartment.copyGeocodedData = function(ApartmentIntstance){
-                var oldData = ApartmentIntstance.apartmentData;
-                return new Apartment(
-                    oldData.id,
-                    oldData.street,
-                    null,
-                    oldData.neighborhood,
-                    oldData.locality,
-                    oldData.administrative_area_level_3,
-                    oldData.state,
-                    oldData.zip,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    oldData.latitude,
-                    oldData.longitude,
-                    oldData.concatAddr,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null
-                );
-            };
-            Apartment.build = function(data) {
-                return new Apartment(
-                    data.id,
-                    data.street,
-                    data.unitNum,
-                    data.neighborhood,
-                    data.locality,
-                    data.administrative_area_level_3,
-                    data.state,
-                    data.zip,
-                    data.beds,
-                    data.baths,
-                    data.livingSpaces,
-                    data.maxResidency,
-                    data.costPerMonth,
-                    data.renovated,
-                    data.pets,
-                    data.ApartmentType,
-                    data.latitude,
-                    data.longitude,
-                    data.concatAddr,
-                    data.leaseType,
-                    data.leaseExpectedEnd,
-                    data.leaseActualEnd,
-                    data.PropertyManagerId,
-                    data.route,
-                    data.laundry,
-                    data.elevator,
-                    data.gym,
-                    data.pool,
-                    data.parking,
-                    data.concierge,
-                    data.airconditioning,
-                    data.roofDeck,
-                    data.superIntendent,
-                    data.balcony,
-                    data.garage,
-                    data.CreatedById,
-                    data.UpdatedById,
-                    data.pubid,
-                    data.ActiveListings
-                );
-            };
-
-
-            return Apartment;
+          );
+        },
+        getById: function(callback) {
+          $resource(WizioConfig.baseAPIURL + "apartment/:id", {
+            id: "@id"
+          }).get(
+            {
+              id: this.id
+            },
+            function(response) {
+              return callback(response);
+            }
+          );
+        },
+        findOrCreate: function(action, callback) {
+          $resource(WizioConfig.baseAPIURL + "apartment/claim/:action", {
+            action: "@action"
+          }).save(action, apartment, function(response) {
+            return callback(response);
+          });
         }
-    ]);
+      };
+    };
+    Apartment.prototype.getGeocodeData = function(callback) {
+      var apartmentData = this.apartmentData;
+      return $q(function(resolve, reject) {
+        // UnitCreateSvc.parseGeocodeData(apartmentData.concatAddr, null, function(err, response) {
+        //   for (var key in response) {
+        //     if (response[key] === "Longitude") {
+        //       apartment.Longitude = parseFloat(response[key]).toFixed(6);
+        //     } else if (response[key] === "Latitude") {
+        //       apartment.Latitude = parseFloat(response[key]).toFixed(6);
+        //     } else {
+        //       apartmentData[key] = response[key];
+        //     }
+        //   }
+        //   resolve("done");
+        // });
+      });
+    };
+    Apartment.prototype.duplicate = function() {
+      var duplicate = {};
+      for (var key in this.apartmentData) {
+        if (this.apartmentData.hasOwnProperty(key)) {
+          var count = 0;
+          if (key !== "id") {
+            duplicate[key] = this.apartmentData[key];
+          }
+        }
+      }
+      return duplicate;
+    };
+    Apartment.setDescription = function() {
+      if (arguments.length > 0) {
+        this.apartmentData.Descriptions = null;
+        this.apartmentData.Descriptions.description = arguments.description;
+        // this.apartmentData.Descriptions.UserId
+      }
+    };
+    Apartment.prototype.concealAddress = function() {
+      var left = Math.floor(this.apartmentData.concatAddr.charCodeAt(5) / 19 + 4);
+      var right = Math.floor(this.apartmentData.concatAddr.charCodeAt(3) / 19 + 4);
+      var houseNumInt = parseInt(this.apartmentData.concatAddr.replace(/(^\d+)(.+$)/i, "$1"));
+      var houseNumLow = houseNumInt - left;
+      if (houseNumInt < 15) {
+        houseNumLow = 1;
+      }
+      var houseNumHigh = houseNumInt + right;
+      var houseNumRange = houseNumLow.toString() + "-" + houseNumHigh.toString();
+      this.apartmentData.concatAddr = houseNumRange + this.apartmentData.concatAddr.replace(/^\d+/, "");
+    };
+    // Apartment.prototype.api = function() {
+    //     return {
+    //         oneParam: $resource(WizioConfig.baseAPIURL + 'apartment/:id')
+    //     };
+    // };
+    // Apartment.prototype.api = function() {
+    //     return $resource(WizioConfig.baseAPIURL + 'apartment/:id', {
+    //         id: '@id'
+    //     });
+    // };
+    Apartment.api = function() {
+      return $resource(WizioConfig.baseAPIURL + "apartment/:id", {
+        id: "@id"
+      });
+    };
+    Apartment.claimApi = function(data, callback) {
+      return $resource(WizioConfig.baseAPIURL + "apartment/claim/finalize").save(data, function(res) {
+        return callback(res);
+      });
+    };
+
+    Apartment.copyGeocodedData = function(ApartmentIntstance) {
+      var oldData = ApartmentIntstance.apartmentData;
+      return new Apartment(
+        oldData.id,
+        oldData.street,
+        null,
+        oldData.neighborhood,
+        oldData.locality,
+        oldData.administrative_area_level_3,
+        oldData.state,
+        oldData.zip,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        oldData.latitude,
+        oldData.longitude,
+        oldData.concatAddr,
+        null,
+        null,
+        null,
+        null,
+        null
+      );
+    };
+    Apartment.build = function(data) {
+      return new Apartment(
+        data.id,
+        data.street,
+        data.unitNum,
+        data.neighborhood,
+        data.locality,
+        data.administrative_area_level_3,
+        data.state,
+        data.zip,
+        data.beds,
+        data.baths,
+        data.livingSpaces,
+        data.maxResidency,
+        data.costPerMonth,
+        data.renovated,
+        data.pets,
+        data.ApartmentType,
+        data.latitude,
+        data.longitude,
+        data.concatAddr,
+        data.leaseType,
+        data.leaseExpectedEnd,
+        data.leaseActualEnd,
+        data.PropertyManagerId,
+        data.route,
+        data.laundry,
+        data.elevator,
+        data.gym,
+        data.pool,
+        data.parking,
+        data.concierge,
+        data.airconditioning,
+        data.roofDeck,
+        data.superIntendent,
+        data.balcony,
+        data.garage,
+        data.CreatedById,
+        data.UpdatedById,
+        data.pubid,
+        data.ActiveListings
+      );
+    };
+
+    return Apartment;
+  }
+]);
