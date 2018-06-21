8ab7c032abb7f24d4beb815c806338d787368992
Cameron Billings <Cameron.billings92@gmail.com>
2018-06-20 21:46:30 -0400

    Homepage update june (#676)
    * copy updates
    
    * Button forwarding
    
    * change buttons to form open in a new tab
    
    * responsive adjustments
    
    * updating app version

--- public/app/modules/landing-page-app/main/landing-page.ctrl.js
+++ public/app/modules/landing-page-app/main/landing-page.ctrl.js
@@ -14,5 +14,15 @@ angular.module('LandingPageApp')
             $scope.goToProduct = function (){
                 $state.go('Product');
             };
+
+            $scope.openPopUp = function (){
+              console.log("clicked")
+              require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us10.list-manage.com","uuid":"dd1716a846e7e35a09a1562a9","lid":"973bf1b4ca"}) })
+
+            };
+
+
         }
+
+
     ])
