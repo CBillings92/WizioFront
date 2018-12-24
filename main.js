b577553628f27484ff00049b779cba5a4a1cddbe
Cameron <cameron.billings92@gmail.com>
2018-12-24 11:25:50 -0500

    adding compression

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ var favicon = require("serve-favicon");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+var compression = require("compression");
 // var config = require('./config/config');
 
 var routes = require("./routes/index");
 
 var app = express();
+app.use(compression());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -21,7 +23,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/public", express.static(path.join(__dirname, "public")));
-app.use("/bower_components", express.static(path.join(__dirname, "bower_components")));
+app.use(
+  "/bower_components",
+  express.static(path.join(__dirname, "bower_components"))
+);
 app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 
 app.use("/", routes);
