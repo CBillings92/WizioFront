c7c3622b23f7a26261bd0714c543d1bb54f4bb5f
Cameron Billings <Cameron.billings92@gmail.com>
2018-12-30 22:03:36 -0500

    Compression (#691)
    * adding compression
    
    * updating compression and app version

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ var favicon = require("serve-favicon");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+var compression = require("compression");
 // var config = require('./config/config');
 
 var routes = require("./routes/index");
 
 var app = express();
-// app.use(compression());
+app.use(compression());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
