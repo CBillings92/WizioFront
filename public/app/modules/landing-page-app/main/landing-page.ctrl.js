b3c5d272e70c64cedb43fc316f2c7c3d4eea6ec6
Cameron <cameron.billings92@gmail.com>
2019-01-01 22:16:54 -0500

    adding conditional to show or not show search based on who is logged in

--- public/app/modules/landing-page-app/main/landing-page.ctrl.js
+++ public/app/modules/landing-page-app/main/landing-page.ctrl.js
@@ -4,7 +4,14 @@
 angular.module("LandingPageApp").controller("LandingPageCtrl", [
   "$scope",
   "$state",
-  function($scope, $state) {
+  "TokenSvc",
+  function($scope, $state, TokenSvc) {
+    $scope.loggedInUser = "";
+
+    if (TokenSvc.isLoggedIn() && TokenSvc.decode().Subscriptions[0].id === 3) {
+      $scope.loggedInUser = "wizio";
+    }
+    console.dir($scope.loggedInUser);
     $scope.goToDashboard = function() {
       $state.go("Account.Dashboard");
     };
@@ -16,7 +23,11 @@ angular.module("LandingPageApp").controller("LandingPageCtrl", [
     $scope.openPopUp = function() {
       console.log("clicked");
       require(["mojo/signup-forms/Loader"], function(L) {
-        L.start({ baseUrl: "mc.us10.list-manage.com", uuid: "dd1716a846e7e35a09a1562a9", lid: "973bf1b4ca" });
+        L.start({
+          baseUrl: "mc.us10.list-manage.com",
+          uuid: "dd1716a846e7e35a09a1562a9",
+          lid: "973bf1b4ca"
+        });
       });
     };
   }
