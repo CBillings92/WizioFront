9dc1480c2f38bbfbafc6bccc3985f3a1b68f5422
Cameron <cameron.billings92@gmail.com>
2019-01-04 23:13:10 -0500

    updating search

--- config/local.js
+++ config/local.js
@@ -1,5 +1,5 @@
 module.exports = {
-  backendAPIURL: "http://172.18.0.3:4000/",
+  backendAPIURL: "http://172.19.0.3:4000/",
   s3bucketURL: "https://d1mze0h82dkhhe.cloudfront.net/",
   frontendURL: "http://localhost:3000/"
 };
